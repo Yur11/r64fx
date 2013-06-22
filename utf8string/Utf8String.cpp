aa4a4e2a02a983643aa94708f56c318fb679c7e8
Yuri Klopovsky <yuri.klopovsky@gmail.com>
2013-06-22 17:09:59 +0300

    Added several libraries.
       - utf8string
       - json
       - serialize

---
+++ utf8string/Utf8String.cpp
@@ -0,0 +1,99 @@
+#include "Utf8String.h"
+
+std::string Utf8String::operator[](unsigned int index)
+{
+    int byte = bytes_up_to(index);
+    int nbytes = next_utf8_char(byte) - byte;
+    return std::string(stdstr, byte, nbytes);
+}
+
+
+int Utf8String::next_utf8_char(int index)
+{
+    char c = stdstr[index];
+    if(!(c & 128))//ascii byte
+    {
+        index++;
+    }
+    else
+    {
+        //Count leading 1s.
+        int leading_one_count = 1;//We know that we have at least one leading 1 by now.
+        for(int i=6; i>=0; i--)
+        {
+            if(!(c & (1<<i))) break;
+            leading_one_count++;
+        }
+
+        /* Invalid utf-8 */
+        if(leading_one_count < 2)  return -1;
+        if(index + leading_one_count > (int)stdstr.size()) return -1;
+
+        
+        index++;
+        
+        /* Check the validity of each of the continuation bytes. */
+        int continuation_byte_count = leading_one_count-1;
+        for(int i=0; i<continuation_byte_count; i++)
+        {
+            c = stdstr[index];
+            if((c & (1<<7)) && ((~c) & (1<<6))) //must be 10xxxxxx
+                index++;
+            else               
+                return -1;
+        }
+    }
+    
+    return index;
+}
+
+
+int Utf8String::size()
+{   
+    int result = 0;
+    int i=0;
+    while(i != (int)stdstr.size())
+    {
+        i = next_utf8_char(i);
+        if(i == -1) return -1;
+        result++;
+    }
+    
+    return result;
+}
+
+
+
+int Utf8String::bytes_from_to(int first_byte, int ncharacters)
+{
+    int byte = first_byte;
+    int n = 0;
+    while(n++ < ncharacters)
+    { 
+        byte = next_utf8_char(byte); 
+        if(byte == -1) return -1;
+    }
+    return byte;
+}
+
+
+void Utf8String::insert(int position, Utf8String other)
+{
+    int byte = bytes_up_to(position);
+    stdstr.insert(byte, other.stdstr);
+}
+
+
+void Utf8String::insert(int position, int n, std::string ch)
+{
+    Utf8String tmp;
+    while(n--) tmp.stdstr += ch;
+    insert(position, tmp);
+}
+
+void Utf8String::erase(int position, int n)
+{
+    int begin_byte = bytes_up_to(position);
+    int end_byte = bytes_from_to(begin_byte, n);
+    stdstr.erase(begin_byte, end_byte - begin_byte);
+}
---
+++ utf8string/Utf8String.h
@@ -0,0 +1,54 @@
+#ifndef UTF_8_STRING_H
+#define UTF_8_STRING_H
+
+#include <string>
+
+/** @brief Utf-8 wrapper for std::string. */
+class Utf8String{
+public:
+    std::string stdstr;
+    
+    Utf8String(std::string str = "") : stdstr(str) {}
+    
+    Utf8String(const char* cstr) : Utf8String(std::string(cstr)) {}
+    
+    inline const char* c_str() { return stdstr.c_str(); }
+    
+    std::string operator[](unsigned int index);
+    
+    /** @brief First byte index of the next utf-8 character after the character starting at the given byte.
+     
+        In case of invalid utf-8 encoding function returns -1.
+     */
+    int next_utf8_char(int index);
+    
+    /** @brief Number of utf-8 characters in the string.
+     
+        In case of invalid utf-8 encoding function returns -1.
+     */
+    int size();
+    
+    /** @brief The number of bytes from the beginning of the string up to the given character index. 
+      
+        In case of invalid utf-8 encoding function returns -1.
+     */
+    inline int bytes_up_to(int index) { return bytes_from_to(0, index); }
+   
+    /** @brief The number of bytes from the first_byte index up to the given number of characters. 
+     
+        In case of invalid utf-8 encoding function returns -1.
+     */
+    int bytes_from_to(int first_byte, int ncharacters);
+    
+    /** @brief Insert substring at the given position. */
+    void insert(int position, Utf8String other);
+   
+    /** @brief Insert n instances of ch at the given position. */
+    void insert(int position, int n, std::string ch);
+    
+    /** @brief Erase n utf-8 characters starting at the utf-8 character at the given position. */
+    void erase(int position, int n);
+};
+
+
+#endif//UTF_8_STRING_H
