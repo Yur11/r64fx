552dc9019848ad3b33cbfb5424defdedba97110a
Yuri Klopovsky <yuri.klopovsky@gmail.com>
2013-10-26 20:27:39 +0300

    Reworked bezier curves.
    - Added some documentation to bezier.h.
    
    - Using a recursive algorithm instead of iterative one.
      Should not be much of a performance penalty, if a small
      number of control points is used.

--- gui/bezier.cpp
+++ gui/bezier.cpp
@@ -1,9 +1,5 @@
 #include "bezier.h"
 
-#ifdef DEBUG
-#include <assert.h>
-#endif//DEBUG
-
 using namespace std;
 
 namespace r64fx{
@@ -16,35 +12,21 @@ Point<float> point_on_a_segment(Point<float> a, Point<float> b, float position)
 }
 
 
-
-void bezier_points(const std::vector<Point<float>> &in, std::vector<Point<float>> &out)
+Point<float> bezier_point(const std::vector<Point<float>> &in, float position)
 {
 #ifdef DEBUG
     assert(in.size() >= 2);
-    assert(out.size() >= 1);
 #endif//DEBUG
     
-    float step = 1.0 / out.size();
-    float position = step;
-
-    out.back() = in.back();
-    out.front() = in.front();
-
-    vector<Point<float>> intermadiate_points = in;
-    for(int i=1; i<(int)out.size()-1; i++)
-    {
-        int n = intermadiate_points.size() - 1;
-        while(n > 1)
-        {
-            for(int j=0; j<n; j++)
-            {
-                intermadiate_points[j] = point_on_a_segment(intermadiate_points[j], intermadiate_points[j+1], position);
-            }
-            n--;
-        }
-        out[i] = point_on_a_segment(intermadiate_points[0], intermadiate_points[1], position);
-        position += step;
-    }
+    if(in.size() == 2)
+        return point_on_a_segment(in[0], in[1], position);
+        
+    vector<Point<float>> vec;
+    for(int i=0; i<(int)in.size()-1; i++)
+        vec.push_back(point_on_a_segment(in[i], in[i+1], position));
+    
+    return bezier_point(vec, position);
 }
+
     
 }//namespace r64fx
