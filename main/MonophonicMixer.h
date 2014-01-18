e920c0758d80c3966ff936864b368fd8ddac4f47
Yuri Klopovsky <yuri.klopovsky@gmail.com>
2014-01-19 01:50:29 +0200

    Added a simple monophonic mixer.

---
+++ main/MonophonicMixer.h
@@ -0,0 +1,29 @@
+#ifndef R64FX_MAIN_MONOPHONIC_MIXER_H
+#define R64FX_MAIN_MONOPHONIC_MIXER_H
+
+#include "dsp/Processor.h"
+
+namespace r64fx{
+    
+class MonophonicMixer : public Processor{
+    InputPort _input_port;
+    InputPort _level_port;
+    float* _mix;
+    
+public:
+    MonophonicMixer();
+    
+   ~MonophonicMixer();
+   
+    virtual void render(Assembler &as);
+    
+    inline InputPort* inputPort() { return &_input_port; }
+    
+    inline InputPort* levelPort() { return &_level_port; }
+    
+    inline float* mix() { return _mix; }
+};
+    
+}//namespace r64fx
+
+#endif//R64FX_MAIN_MONOPHONIC_MIXER_H
