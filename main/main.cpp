babe71f37874e48c92819ea4332a1cb26af38612
Yuri Klopovsky <yuri.klopovsky@gmail.com>
2014-01-30 14:21:54 +0200

    Moving to shader and vao rendering.

--- main/main.cpp
+++ main/main.cpp
@@ -16,12 +16,12 @@
 #include "gui/Dummy.h"
 #include "gui/TextEdit.h"
 #include "gui/Keyboard.h"
+#include "gui/TexturedRect.h"
 #include "gui/Font.h"
 #include "gc/gc.h"
 #include "json/Json.h"
 #include "Machine.h"
 #include "gui/TextureAtlas.h"
-
 #include "gui/geometry_io.h"
 
 #include "dsp/Processor.h"
@@ -29,7 +29,7 @@
 #include "MonophonicMixer.h"
 
 #include "knobs.h"
-#include "sockets_and_wires.h"
+#include "Wire.h"
 
 using namespace std;
 
@@ -45,23 +45,8 @@ typedef SDL2Window Window_t;
 
 string data_prefix;
 
-    
-/* Default font is built into the binary. 
- * Just in case the data paths are really messed up
- * and we want to render text.
- */    
-
-const vector<unsigned char> jura_book_font = {
-    #include "jura_book_font.ttf.h"
-};
-
-
-/** For widgets that render shadows and stuff.
- *  Import it with extern.
- *  Use only in the main module. 
- */
-float light_angle = 0.75 * M_PI;
-
+/** Removeme */
+Font* debug_font = nullptr;
 
 /** @brief Main program class. 
  
@@ -130,28 +115,23 @@ struct Program{
         /* Can be done only after we have obtained the OpenGL context. */
         if(!Window::initGlew())
             return 3;
+
+        Keyboard::init();
         
         Texture::init();
+
+        TexturedRect::init();
         
-        Keyboard::init();
+        Socket::init();
         
         Wire::init();
         
-        /* Initialize default font. */
-        Font font(jura_book_font.data(), jura_book_font.size());
-        font.setFaceSize(16);
-        if(font.isOk())
-        {
-            Font::initDefaultFont(&font);
-        }
-        else
-        {
-            cerr << "Problem creating default font!\n";
-            abort();
-        }
+        Font::init();
+        debug_font = new Font(data_prefix + "fonts/FreeSans.ttf", 13);
+        assert(debug_font->isOk());
 
         /* Set the UI language. */
-        tr.loadLanguage("ru");
+        tr.loadLanguage("en");
         
         /* removeme. */
         Dummy::initDebugMenu();
@@ -178,7 +158,7 @@ struct Program{
         Machine* m3 = new Machine(&fms, &bms);
         m3->setPosition(100, 500);
 
-        TexturedKnobBackground txkbg("textures/knob_bg.png");
+        TexturedKnobBackground txkbg(data_prefix + "textures/knob_bg.png");
         KnobHandleTypeA::init();
         KnobHandleTypeA khta;
         
@@ -233,202 +213,197 @@ struct Program{
         auto sa = new Socket;
         sa->setPosition(10, 10);
         sa->update();
-        sa->is_plugged = true;
         m2->back()->appendWidget(sa);
         
         auto sb = new Socket;
         sb->setPosition(400, 10);
         sb->update();
-        sb->is_plugged = true;
         m3->back()->appendWidget(sb);
         
-        Wire* wire = new Wire;
-        wire->setA(sa);
-        wire->setB(sb);
+        Wire* wire = new Wire(sa, sb);
         wire->update();
         wire->color = { 0.7, 0.7, 0.1, 0.0 };
         wires.push_back(wire);
         
         /* Setup View icons. */
-        View::split_view_vert_icon = Icon({24, 24}, "textures/split_vertically.png");
-        View::split_view_hor_icon  = Icon({24, 24}, "textures/split_horizontally.png");
-        View::close_view_icon      = Icon({24, 24}, "textures/close_view.png");
+        View::split_view_vert_icon = Icon({24, 24}, data_prefix + "textures/split_vertically.png");
+        View::split_view_hor_icon  = Icon({24, 24}, data_prefix + "textures/split_horizontally.png");
+        View::close_view_icon      = Icon({24, 24}, data_prefix + "textures/close_view.png");
         
         /* Setup root view of the main window. */        
         View* view = new View(&fms);
         window.setView(view);
+// 
+//         
+//         int max_texture_size;
+//         glGetIntegerv(GL_MAX_TEXTURE_SIZE, &max_texture_size);
+        
+//         jack_status_t jack_status;
+//         jack_client = jack_client_open("r64fx", JackNullOption, &jack_status);
+//         if(!jack_client)
+//         {
+//             cerr << "Failed to create jack client!\n";
+//             abort();
+//         }
+//         
+//         input_port = jack_port_register(jack_client, "in", JACK_DEFAULT_AUDIO_TYPE, JackPortIsInput, 0);
+//         if(!input_port)
+//         {
+//             cerr << "Failed to create input port!\n";
+//             abort();
+//         }
+//         
+//         output_port = jack_port_register(jack_client, "out", JACK_DEFAULT_AUDIO_TYPE, JackPortIsOutput, 0);
+//         if(!output_port)
+//         {
+//             cerr << "Failed to create output port!\n";
+//             abort();
+//         }
+//         
+//         midi_input_port = jack_port_register(jack_client, "midi_in", JACK_DEFAULT_MIDI_TYPE, JackPortIsInput, 0);
+//         if(!midi_input_port)
+//         {
+//             cerr << "Failed to create midi input port!\n";
+//             abort();
+//         }
+        
+//         audio_buffer_size = jack_get_buffer_size(jack_client);
+//         
+//         input_buffer = new float[audio_buffer_size];
+//         output_buffer = new float[audio_buffer_size];
+//         for(auto i=0; i<audio_buffer_size; i++)
+//         {
+//             input_buffer[i] = output_buffer[i] = 0.0;
+//         }
+//         
+//         jack_set_process_thread(jack_client, [](void* data)->void*{
+//             auto program = (Program*) data;
+//             program->jack_thread();
+//             return nullptr;
+//         }, this);
+//         
+        
+//         SawtoothOsc osc;
+//         auto osc_slot1 = osc.getSlot();
+//         assert(osc_slot1 != nullptr);
+//         auto osc_slot2 = osc.getSlot();
+//         assert(osc_slot2 != nullptr);
+//         auto osc_slot3 = osc.getSlot();
+//         assert(osc_slot3 != nullptr);
+//         auto osc_slot4 = osc.getSlot();
+//         assert(osc_slot4 != nullptr);
+//         
+//         freq1_addr = osc.frequencyPort()->at(osc_slot1);
+//         freq2_addr = osc.frequencyPort()->at(osc_slot2);
+//         freq3_addr = osc.frequencyPort()->at(osc_slot3);
+//         freq4_addr = osc.frequencyPort()->at(osc_slot4);
+        
+//         MonophonicMixer mix;
+//         auto mix_slot1 = mix.getSlot();
+//         assert(mix_slot1 != nullptr);
+//         auto mix_slot2 = mix.getSlot();
+//         assert(mix_slot2 != nullptr);
+//         auto mix_slot3 = mix.getSlot();
+//         assert(mix_slot3 != nullptr);
+//         auto mix_slot4 = mix.getSlot();
+//         assert(mix_slot4 != nullptr);
+//         
+//         level1_addr = mix.levelPort()->at(mix_slot1);
+//         level2_addr = mix.levelPort()->at(mix_slot2);
+//         level3_addr = mix.levelPort()->at(mix_slot3);
+//         level4_addr = mix.levelPort()->at(mix_slot4);
+//         
+//         k1->value_changed = Message([](void* source, void* data) -> void*{
+//             BasicKnob* knob = (BasicKnob*) source;
+//             float* freq = (float*) data;
+//             float f = knob->value();
+//             *freq = f;
+//             return nullptr;
+//         }, freq1_addr);
+//         
+//         k2->value_changed = Message([](void* source, void* data) -> void*{
+//             BasicKnob* knob = (BasicKnob*) source;
+//             float* level = (float*) data;
+//             float l = knob->value();
+//             *level = l;
+//             return nullptr;
+//         }, level1_addr);
+//         
+//         k3->value_changed = Message([](void* source, void* data) -> void*{
+//             BasicKnob* knob = (BasicKnob*) source;
+//             float* freq = (float*) data;
+//             float f = knob->value();
+//             *freq = f;
+//             return nullptr;
+//         }, freq2_addr);
+//         
+//         k4->value_changed = Message([](void* source, void* data) -> void*{
+//             BasicKnob* knob = (BasicKnob*) source;
+//             float* level = (float*) data;
+//             float l = knob->value();
+//             *level = l;
+//             return nullptr;
+//         }, level2_addr);
+//         
+//         k5->value_changed = Message([](void* source, void* data) -> void*{
+//             BasicKnob* knob = (BasicKnob*) source;
+//             float* freq = (float*) data;
+//             float f = knob->value();
+//             *freq = f;
+//             return nullptr;
+//         }, freq3_addr);
+//         
+//         k6->value_changed = Message([](void* source, void* data) -> void*{
+//             BasicKnob* knob = (BasicKnob*) source;
+//             float* level = (float*) data;
+//             float l = knob->value();
+//             *level = l;
+//             return nullptr;
+//         }, level3_addr);
+//         
+//         k7->value_changed = Message([](void* source, void* data) -> void*{
+//             BasicKnob* knob = (BasicKnob*) source;
+//             float* freq = (float*) data;
+//             float f = knob->value();
+//             *freq = f;
+//             return nullptr;
+//         }, freq4_addr);
+//         
+//         k8->value_changed = Message([](void* source, void* data) -> void*{
+//             BasicKnob* knob = (BasicKnob*) source;
+//             float* level = (float*) data;
+//             float l = knob->value();
+//             *level = l;
+//             return nullptr;
+//         }, level4_addr);
+//         
+//         auto c1 = make_connection(osc.normalizedPort(), osc_slot1, mix.inputPort(), mix_slot1);
+//         auto c2 = make_connection(osc.normalizedPort(), osc_slot2, mix.inputPort(), mix_slot2);
+//         auto c3 = make_connection(osc.normalizedPort(), osc_slot3, mix.inputPort(), mix_slot3);
+//         auto c4 = make_connection(osc.valuePort(), osc_slot4, osc.fmPort(), osc_slot1);
+//         auto c5 = make_connection(osc.valuePort(), osc_slot4, osc.fmPort(), osc_slot2);
+//         auto c6 = make_connection(osc.valuePort(), osc_slot4, osc.fmPort(), osc_slot3);
+//         
+//         graph->processors.push_back(&osc);
+//         graph->processors.push_back(&mix);
+//         
+//         graph->writeToBuffer(output_buffer, mix.mix());
+//         
+//         graph->render(audio_buffer_size);
+//         
+//         cout << graph->debug() << "\n";
 
-        
-        int max_texture_size;
-        glGetIntegerv(GL_MAX_TEXTURE_SIZE, &max_texture_size);
-        
-        glEnable(GL_MULTISAMPLE_ARB);
+//         jack_activate(jack_client);
 
-        jack_status_t jack_status;
-        jack_client = jack_client_open("r64fx", JackNullOption, &jack_status);
-        if(!jack_client)
-        {
-            cerr << "Failed to create jack client!\n";
-            abort();
-        }
-        
-        input_port = jack_port_register(jack_client, "in", JACK_DEFAULT_AUDIO_TYPE, JackPortIsInput, 0);
-        if(!input_port)
-        {
-            cerr << "Failed to create input port!\n";
-            abort();
-        }
-        
-        output_port = jack_port_register(jack_client, "out", JACK_DEFAULT_AUDIO_TYPE, JackPortIsOutput, 0);
-        if(!output_port)
-        {
-            cerr << "Failed to create output port!\n";
-            abort();
-        }
-        
-        midi_input_port = jack_port_register(jack_client, "midi_in", JACK_DEFAULT_MIDI_TYPE, JackPortIsInput, 0);
-        if(!midi_input_port)
-        {
-            cerr << "Failed to create midi input port!\n";
-            abort();
-        }
-        
-        audio_buffer_size = jack_get_buffer_size(jack_client);
-        
-        input_buffer = new float[audio_buffer_size];
-        output_buffer = new float[audio_buffer_size];
-        for(auto i=0; i<audio_buffer_size; i++)
-        {
-            input_buffer[i] = output_buffer[i] = 0.0;
-        }
-        
-        jack_set_process_thread(jack_client, [](void* data)->void*{
-            auto program = (Program*) data;
-            program->jack_thread();
-            return nullptr;
-        }, this);
-        
-        
-        SawtoothOsc osc;
-        auto osc_slot1 = osc.getSlot();
-        assert(osc_slot1 != nullptr);
-        auto osc_slot2 = osc.getSlot();
-        assert(osc_slot2 != nullptr);
-        auto osc_slot3 = osc.getSlot();
-        assert(osc_slot3 != nullptr);
-        auto osc_slot4 = osc.getSlot();
-        assert(osc_slot4 != nullptr);
-        
-        freq1_addr = osc.frequencyPort()->at(osc_slot1);
-        freq2_addr = osc.frequencyPort()->at(osc_slot2);
-        freq3_addr = osc.frequencyPort()->at(osc_slot3);
-        freq4_addr = osc.frequencyPort()->at(osc_slot4);
-        
-        MonophonicMixer mix;
-        auto mix_slot1 = mix.getSlot();
-        assert(mix_slot1 != nullptr);
-        auto mix_slot2 = mix.getSlot();
-        assert(mix_slot2 != nullptr);
-        auto mix_slot3 = mix.getSlot();
-        assert(mix_slot3 != nullptr);
-        auto mix_slot4 = mix.getSlot();
-        assert(mix_slot4 != nullptr);
-        
-        level1_addr = mix.levelPort()->at(mix_slot1);
-        level2_addr = mix.levelPort()->at(mix_slot2);
-        level3_addr = mix.levelPort()->at(mix_slot3);
-        level4_addr = mix.levelPort()->at(mix_slot4);
-        
-        k1->value_changed = Message([](void* source, void* data) -> void*{
-            BasicKnob* knob = (BasicKnob*) source;
-            float* freq = (float*) data;
-            float f = knob->value();
-            *freq = f;
-            return nullptr;
-        }, freq1_addr);
-        
-        k2->value_changed = Message([](void* source, void* data) -> void*{
-            BasicKnob* knob = (BasicKnob*) source;
-            float* level = (float*) data;
-            float l = knob->value();
-            *level = l;
-            return nullptr;
-        }, level1_addr);
-        
-        k3->value_changed = Message([](void* source, void* data) -> void*{
-            BasicKnob* knob = (BasicKnob*) source;
-            float* freq = (float*) data;
-            float f = knob->value();
-            *freq = f;
-            return nullptr;
-        }, freq2_addr);
-        
-        k4->value_changed = Message([](void* source, void* data) -> void*{
-            BasicKnob* knob = (BasicKnob*) source;
-            float* level = (float*) data;
-            float l = knob->value();
-            *level = l;
-            return nullptr;
-        }, level2_addr);
-        
-        k5->value_changed = Message([](void* source, void* data) -> void*{
-            BasicKnob* knob = (BasicKnob*) source;
-            float* freq = (float*) data;
-            float f = knob->value();
-            *freq = f;
-            return nullptr;
-        }, freq3_addr);
-        
-        k6->value_changed = Message([](void* source, void* data) -> void*{
-            BasicKnob* knob = (BasicKnob*) source;
-            float* level = (float*) data;
-            float l = knob->value();
-            *level = l;
-            return nullptr;
-        }, level3_addr);
-        
-        k7->value_changed = Message([](void* source, void* data) -> void*{
-            BasicKnob* knob = (BasicKnob*) source;
-            float* freq = (float*) data;
-            float f = knob->value();
-            *freq = f;
-            return nullptr;
-        }, freq4_addr);
-        
-        k8->value_changed = Message([](void* source, void* data) -> void*{
-            BasicKnob* knob = (BasicKnob*) source;
-            float* level = (float*) data;
-            float l = knob->value();
-            *level = l;
-            return nullptr;
-        }, level4_addr);
-        
-        auto c1 = make_connection(osc.normalizedPort(), osc_slot1, mix.inputPort(), mix_slot1);
-        auto c2 = make_connection(osc.normalizedPort(), osc_slot2, mix.inputPort(), mix_slot2);
-        auto c3 = make_connection(osc.normalizedPort(), osc_slot3, mix.inputPort(), mix_slot3);
-        auto c4 = make_connection(osc.valuePort(), osc_slot4, osc.fmPort(), osc_slot1);
-        auto c5 = make_connection(osc.valuePort(), osc_slot4, osc.fmPort(), osc_slot2);
-        auto c6 = make_connection(osc.valuePort(), osc_slot4, osc.fmPort(), osc_slot3);
-        
-        graph->processors.push_back(&osc);
-        graph->processors.push_back(&mix);
-        
-        graph->writeToBuffer(output_buffer, mix.mix());
-        
-        graph->render(audio_buffer_size);
-        
-        cout << graph->debug() << "\n";
-
-        jack_activate(jack_client);
+        glEnable(GL_MULTISAMPLE);
         
         /* Main event loop. */
         int gc_counter = 256;
         for(;;)
         {
             Window_t::processEvents();
-
-            //Process other stuff here.
-
+            
+            window.makeCurrent();
             window.render();
             window.swapBuffers();
             
@@ -445,17 +420,17 @@ struct Program{
             usleep(300);
         }
 
-        jack_deactivate(jack_client);
+//         jack_deactivate(jack_client);
         
-        delete[] input_buffer;
-        delete[] output_buffer;
-
-        while(!cycle_waiting)
-            usleep(100);
+//         delete[] input_buffer;
+//         delete[] output_buffer;
+// 
+//         while(!cycle_waiting)
+//             usleep(100);
 
         /* Cleanup things up before exiting. */
-        jack_client_close(jack_client);
-        delete graph;
+//         jack_client_close(jack_client);
+//         delete graph;
         Texture::cleanup();
         Window_t::cleanup();
         
