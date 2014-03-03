5ef4740163a8fb91acd88d2486db78417f4a5ea8
Yuri Klopovsky <yuri.klopovsky@gmail.com>
2014-03-03 06:39:36 +0200

    Updated painter. New PainterVertices class is available.

--- gui/Painter.cpp
+++ gui/Painter.cpp
@@ -6,11 +6,14 @@ using namespace std;
 namespace r64fx{
 
 ShadingProgram   Painter::sp;
-GLint            Painter::xyst_attr;
+
+GLint            Painter::position_attr;
+GLint            Painter::tex_coord_attr;
+
 GLint            Painter::sxsytxty_uniform; //Projection2D
 GLint            Painter::color_uniform;
 GLint            Painter::sampler_uniform;
-GLuint           Painter::sampler;
+
 GLuint           Painter::plain_tex;
 
 bool Painter::init()
@@ -28,11 +31,19 @@ bool Painter::init()
         return false;
     }
     
-    xyst_attr = glGetAttribLocation(sp.id(), "xyst");
+    position_attr = glGetAttribLocation(sp.id(), "position");
     CHECK_FOR_GL_ERRORS; 
-    if(xyst_attr == -1)
+    if(position_attr == -1)
     {
-        cerr << "Failed to get xyst_attr location!\n";
+        cerr << "Failed to get position_attr location!\n";
+        return false;
+    }
+    
+    tex_coord_attr = glGetAttribLocation(sp.id(), "tex_coord");
+    CHECK_FOR_GL_ERRORS; 
+    if(tex_coord_attr == -1)
+    {
+        cerr << "Failed to get tex_coord_attr location!\n";
         return false;
     }
     
@@ -44,6 +55,14 @@ bool Painter::init()
         return false;
     }
     
+    sampler_uniform = glGetUniformLocation(sp.id(), "sampler");
+    CHECK_FOR_GL_ERRORS; 
+    if(sampler_uniform == -1)
+    {
+        cerr << "Failed to get sampler_uniform location!\n";
+        return false;
+    }
+    
     color_uniform = glGetUniformLocation(sp.id(), "color");
     CHECK_FOR_GL_ERRORS; 
     if(color_uniform == -1)
@@ -51,14 +70,6 @@ bool Painter::init()
         cerr << "Failed to get color_uniform location!\n";
         return false;
     }
-    
-//     sampler_uniform = glGetUniformLocation(sp.id(), "sampler");
-//     CHECK_FOR_GL_ERRORS; 
-//     if(sampler_uniform == -1)
-//     {
-//         cerr << "Failed to get sampler_uniform location!\n";
-//         return false;
-//     }
     
     unsigned char plain_tex_data[16 * 16 * 4];
     for(int i=0; i<16*16*4; i++)
@@ -70,13 +81,27 @@ bool Painter::init()
     glGenTextures(1, &plain_tex);                                          CHECK_FOR_GL_ERRORS; 
     glBindTexture(GL_TEXTURE_2D, plain_tex);                               CHECK_FOR_GL_ERRORS; 
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);     CHECK_FOR_GL_ERRORS; 
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);     CHECK_FOR_GL_ERRORS; 
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);     CHECK_FOR_GL_ERRORS;
+    glTexParameteri(GL_TEXTURE_2D, GL_GENERATE_MIPMAP, GL_TRUE);           CHECK_FOR_GL_ERRORS;
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 16, 16, 0, GL_RGBA, GL_UNSIGNED_BYTE, plain_tex_data);
     CHECK_FOR_GL_ERRORS;
     
     glBindTexture(GL_TEXTURE_2D, 0);         CHECK_FOR_GL_ERRORS; 
     
     return true;
+}
+
+
+void Painter::setTexture(GLuint tex)
+{
+    glActiveTexture(GL_TEXTURE0);
+    CHECK_FOR_GL_ERRORS;
+    
+    glBindTexture(GL_TEXTURE_2D, tex);
+    CHECK_FOR_GL_ERRORS;
+    
+    glUniform1i(sampler_uniform, 0);
+    CHECK_FOR_GL_ERRORS;
 }
 
 
@@ -87,14 +112,74 @@ Painter::Vertex::Vertex(float x, float y, float s, float t)
 }
 
 
-void Painter::Vertex::setupAttributes()
+void Painter::Vertex::setupAttributes(int nbytes)
 {
-    glEnableVertexAttribArray(xyst_attr);
+    glEnableVertexAttribArray(position_attr);
     CHECK_FOR_GL_ERRORS; 
-    glVertexAttribPointer(xyst_attr, 4, GL_FLOAT, GL_FALSE, 0, 0);
+    glVertexAttribPointer(position_attr, 2, GL_FLOAT, GL_FALSE, sizeof(float)*4, 0);
+    CHECK_FOR_GL_ERRORS; 
+    
+    glEnableVertexAttribArray(tex_coord_attr);
+    CHECK_FOR_GL_ERRORS; 
+    glVertexAttribPointer(tex_coord_attr, 2, GL_FLOAT, GL_FALSE, sizeof(float)*4, (void*)(sizeof(float)*2));
     CHECK_FOR_GL_ERRORS; 
 }
     
+    
+void PainterVertices::setupAttributes(int nbytes)
+{
+    glEnableVertexAttribArray(Painter::position_attr);
+    CHECK_FOR_GL_ERRORS;
+    glVertexAttribPointer(Painter::position_attr, 2, GL_FLOAT, GL_FALSE, 0, 0);
+    CHECK_FOR_GL_ERRORS;
+    
+    glEnableVertexAttribArray(Painter::tex_coord_attr);
+    CHECK_FOR_GL_ERRORS;
+    glVertexAttribPointer(Painter::tex_coord_attr, 2, GL_FLOAT, GL_FALSE, 0, (void*)(long)(nbytes >> 1));
+}
+
+
+void PainterVertices::setData(float *ptr)
+{
+    glBufferSubData(
+        GL_ARRAY_BUFFER,
+        0,
+        nbytes(),
+        ptr
+    );
+    CHECK_FOR_GL_ERRORS;
+}
+
+
+void PainterVertices::setPositions(float* ptr, int nfloats, int offset)
+{
+    glBufferSubData(
+        GL_ARRAY_BUFFER, 
+        offset * sizeof(float),
+        nfloats * sizeof(float), 
+        ptr
+    );
+    CHECK_FOR_GL_ERRORS;
+}
+    
+    
+void PainterVertices::setTexCoords(float* ptr, int nfloats, int offset)
+{
+    glBufferSubData(
+        GL_ARRAY_BUFFER, 
+        nfloats * sizeof(float) + offset * sizeof(float), 
+        nfloats * sizeof(float), 
+        ptr
+    );
+    CHECK_FOR_GL_ERRORS;
+}
+
+
+void PainterVertices::render(GLenum mode, int nvertices, int offset)
+{
+    glDrawArrays(mode, offset, nvertices);
+    CHECK_FOR_GL_ERRORS;
+}
    
 }//namespace r64fx
 
--- gui/Painter.h
+++ gui/Painter.h
@@ -10,13 +10,18 @@
 namespace r64fx{
     
 /** @brief A general purpose painter used to draw a lot of gui elements. */
-class Painter{
+class Painter : public Vertices<Painter>{
+    friend class PainterVertices;
+    
     static ShadingProgram sp;
-    static GLint xyst_attr;
+    
+    static GLint position_attr;
+    static GLint tex_coord_attr;
+    
     static GLint sxsytxty_uniform;
     static GLint color_uniform;
     static GLint sampler_uniform;
-    static GLuint sampler;
+    
     static GLuint plain_tex;
     
 public:
@@ -28,8 +33,10 @@ public:
     public:
         Vertex(float x = 0.0, float y = 0.0, float s = 0.0, float t = 0.0);
         
-        static void setupAttributes();
+        static void setupAttributes(int nbytes);
     };
+    
+    void setupAttributes();
     
     inline static void enable() { sp.use(); };
     
@@ -41,15 +48,39 @@ public:
     
     inline static void setColor(Color color) { glUniform4fv(color_uniform, 1, color.vec); CHECK_FOR_GL_ERRORS;  }
     
-    inline static void setTextureUnit(GLuint tex_unit) { glBindSampler(tex_unit, sampler); CHECK_FOR_GL_ERRORS;  }
+    inline static void setColor(float r, float g, float b, float a) { setColor({r, g, b, a}); }
+    
+    static void setTexture(GLuint tex);
+    
+    inline static void useNoTexture() { setTexture( Painter::plainTexture() ); }
     
     inline static GLuint plainTexture() { return plain_tex; }
     
-    inline static void paint(GLenum mode, int nvertices, int offset = 0) { glDrawArrays(mode, offset, nvertices); CHECK_FOR_GL_ERRORS;  }
-    
-    template<typename T> void paintVertices(GLenum mode, T vertices, int offset = 0) { paint(mode, vertices.size(), offset); }
+    inline static void paint(GLenum mode, int nvertices, int offset = 0) { glDrawArrays(mode, offset, nvertices); CHECK_FOR_GL_ERRORS;  }    
 };
+
+
+/** @brief Vertices to be used with the Painter class. */
+class PainterVertices : public Vertices<PainterVertices>{
+    friend class Vertices<PainterVertices>;
+    static void setupAttributes(int nbytes);
     
+public:
+    PainterVertices(int nvertices)
+    : Vertices<PainterVertices>(nvertices * sizeof(float) * 4)
+    {}
+    
+    void setData(float *ptr);
+    
+    void setPositions(float* ptr, int nfloats, int offset = 0);
+    
+    void setTexCoords(float* ptr, int nfloats, int offset = 0);
+    
+    inline void render(GLenum mode) { render(mode, nbytes()>>4); }
+    
+    void render(GLenum mode, int nvertices, int offset = 0);
+};
+
 }//namespace r64fx
 
 #endif//R64FX_GUI_PAINTER_H
