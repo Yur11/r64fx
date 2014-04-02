8db5610e146bae6ebe692a4aacb28f1522c11717
Yuri Klopovsky <yuri.klopovsky@gmail.com>
2014-04-02 22:50:46 +0300

    Trying Kahan summation with sse commands. Looks good.

--- jit/tryjit.cxx
+++ jit/tryjit.cxx
@@ -7,8 +7,14 @@ using namespace r64fx;
 
 typedef long int (*Fun)(void);
 
-int a[4] = { 0, 0, 0, 0 };
-int b[8] = { 1, 2, 3, 4, -4, -3, 1, -1 };
+int samplerate = 192000;
+
+float sr[4] = { float(samplerate), float(samplerate)*2, float(samplerate)*4, float(samplerate)*8 };
+float zero[4] = { 0, 0, 0, 0 };
+float one[4] = { 1.0, 1.0, 1.0, 1.0 };
+
+float a[4] = { 0, 0, 0, 0 };
+float b[4] = { 0, 0, 0, 0 };
 
 
 template<typename T>
@@ -21,18 +27,51 @@ void dump(T* p)
 
 int main()
 {
+    cout << fixed << setprecision(15);
+    
     CodeBuffer cb;
     Assembler as(cb);
 
-    as.movaps(xmm0, Mem128(a));
-    as.mov(rbx, Imm64(b));
-    as.mov(rcx, Imm64(101010101));
-    auto loop = Mem8(as.ip());
-        as.psubd(xmm0, Base(rbx), Disp8(4*sizeof(int)));
-        as.sub(rcx, 1);
-        as.jnz(loop);
+    /* Calculate reciprocal. */
+    as.movaps(xmm0, Mem128(one));
+    as.divps(xmm0, Mem128(sr));
     as.movaps(Mem128(a), xmm0);
+
+    /** Simple */
+//     as.movaps(xmm1, Mem128(zero));
+//     as.mov(rcx, samplerate);
+//     Mem8 l = as.ip();
+//         as.addps(xmm1, xmm0);
+//         as.sub(rcx, 1);
+//         as.jnz(l);
+
+
+    /** Kahan */
+    as.movaps(xmm1, zero); //sum
+    as.movaps(xmm2, xmm1); //correction
+    as.mov(rcx, samplerate);
+    Mem8 l = as.ip();
+        /* var y = input - c */
+        as.movaps(xmm3, xmm0);
+        as.subps(xmm3, xmm2);
     
+        /* var t = sum + y */
+        as.movaps(xmm4, xmm1);
+        as.addps(xmm4, xmm3);
+        
+        /* c = (t-sum) - y */
+        as.movaps(xmm2, xmm4);
+        as.subps(xmm2, xmm1);
+        as.subps(xmm2, xmm3);
+        
+        /* sum = t */
+        as.movaps(xmm1, xmm4);
+        
+        as.sub(rcx, 1);
+        as.jnz(l);
+
+    as.movaps(b, xmm1);
+        
     as.ret();
     
     Fun fun = (Fun) as.getFun();
