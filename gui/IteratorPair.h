6ebdafce36ed37ecf2284565c090b9f99ba3e0ca
Yuri Klopovsky <yuri.klopovsky@gmail.com>
2014-04-22 13:38:28 +0300

    Added IteratorPair

---
+++ gui/IteratorPair.h
@@ -0,0 +1,24 @@
+#ifndef R64FX_GUI_ITERATOR_PAIR_H
+#define R64FX_GUI_ITERATOR_PAIR_H
+
+namespace r64fx{
+  
+template<typename IteratorT> class IteratorPair{
+    IteratorT _begin, _end;
+    
+public:
+    IteratorPair(IteratorT begin, IteratorT end)
+    : _begin(begin)
+    , _end(end)
+    {}
+    
+    inline IteratorT begin() const { return _begin; }
+    inline IteratorT end() const { return _end; }
+    
+    inline void setBeginIterator(IteratorT it) { _begin = it; }
+    inline void setEndIterator(IteratorT it) { _end = it; }
+};
+    
+}//namespace r64fx
+
+#endif//R64FX_GUI_ITERATOR_PAIR_H
