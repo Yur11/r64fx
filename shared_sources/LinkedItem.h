c4f886b8b7d49fdfff4fa4b77341956057bf5394
Yuri Klopovsky <yuri.klopovsky@gmail.com>
2014-05-03 23:38:39 +0300

    Added LinkedItemChain class. Added some helper functions.

--- shared_sources/LinkedItem.h
+++ shared_sources/LinkedItem.h
@@ -20,9 +20,10 @@ public:
     /** @brief Insert another LinkedItem after this one. */
     void insert(T* item)
     {
-#ifdef DEBUG
+#ifdef DEBUG_LINKED_ITEM_CHAIN
+        assert(item != nullptr);
         assert(item->prev_item == nullptr);
-#endif//DEBUG
+#endif//DEBUG_LINKED_ITEM_CHAIN
         item->prev_item = this;
         if(next_item)
         {
@@ -42,13 +43,23 @@ public:
         next_item = prev_item = nullptr;
     }
     
+    /** @brief Set pointers to nullptr, without affecting the siblings. 
+     
+        Usefull for clearing the list.
+     */
+    void reset()
+    {
+        prev_item = next_item = nullptr;
+    }
+    
     /** @brief Replace this item with another one.*/
     void replace(T* item)
     {
-#ifdef DEBUG
+#ifdef DEBUG_LINKED_ITEM_CHAIN
+        assert(item != nullptr);
         assert(item->prev == nullptr);
         assert(item->next == nullptr);
-#endif//DEBUG
+#endif//DEBUG_LINKED_ITEM_CHAIN
         if(prev_item)
             prev_item->next_item = item;
         if(next_item)
@@ -62,7 +73,7 @@ public:
 };
 
 
-/** */
+/** @brief A wrapper around LinkedItem to be used as an iterator. */
 template<typename T>
 class LinkedItemPointer{
     T* ptr;
@@ -124,6 +135,180 @@ public:
 };
 
 
+struct Item : public LinkedItem<Item>{
+    int num = false;
+    
+    Item(int num)
+    : num(num)
+    {}
+};
+
+
+/** @brief Insert a new item at the end of the list. 
+    
+    Parameters first_ptr and last_ptr are pointers to the first and the last item of the list respectively.
+    They are updated as needed.
+    Their initial value can be nullptr. In that case the list is assumed to be empty.
+    They both must be either nullptr or non nullptr.
+        
+    The function returns the item that has been inserted.
+    It must be a valid object.
+    The item's prev() pointer must be nullptr.
+    If item is the first item of another list, the lists are linked.
+ */
+template<typename T> Item* append_to_list(T* &first_ptr, T* &last_ptr, T* item)
+{
+#ifdef DEBUG_LINKED_ITEM_CHAIN
+    assert(item != nullptr);
+    assert(item->prev() == nullptr);
+    assert(item != first_ptr);
+    assert(item != last_ptr);
+#endif//DEBUG_LINKED_ITEM_CHAIN
+    
+    if(last_ptr == nullptr)
+    {
+#ifdef DEBUG_LINKED_ITEM_CHAIN
+        assert(first_ptr == nullptr);
+#endif//DEBUG_LINKED_ITEM_CHAIN
+        first_ptr = last_ptr = item;
+    }
+    else
+    {
+#ifdef DEBUG_LINKED_ITEM_CHAIN
+        assert(first_ptr != nullptr);
+#endif//DEBUG_LINKED_ITEM_CHAIN
+        last_ptr->insert(item);
+        last_ptr = item;
+    }
+    
+    return item;
+}
+
+
+/** @brief Insert a new item after an existing one. 
+ 
+    Parameters first_ptr and last_ptr are pointers to the first and the last item of the list respectively.
+    They are updated as needed.
+    Their initial value can be nullptr. In that case the list is assumed to be empty.
+    They both must be either nullptr or non nullptr.
+    
+    The existing_item must belong to the list bound by first_ptr and last_ptr.
+    No check is done for this.
+    It can be nullptr only if the list is empty, i.e both first_ptr and last_item are null.
+    In that case it must be null.
+    
+    The function returns the item that has been inserted.
+    It must be a valid object.
+    Also it must not belong to a list.
+ */
+template<typename T> T* insert_to_list(T* &first_ptr, T* &last_ptr, T* existing_item, T* new_item)
+{
+#ifdef DEBUG_LINKED_ITEM_CHAIN
+    assert(new_item != nullptr);
+    assert(new_item->next() == nullptr);
+    assert(new_item->prev() == nullptr);
+    assert(new_item != first_ptr);
+    assert(new_item != last_ptr);
+#endif//DEBUG_LINKED_ITEM_CHAIN
+    
+    if(last_ptr == nullptr)
+    {
+#ifdef DEBUG_LINKED_ITEM_CHAIN
+        assert(first_ptr == nullptr);
+        assert(existing_item == nullptr);
+#endif//DEBUG_LINKED_ITEM_CHAIN
+        first_ptr = last_ptr = new_item;
+    }
+    else
+    {
+#ifdef DEBUG_LINKED_ITEM_CHAIN
+        assert(first_ptr != nullptr);
+        assert(existing_item != nullptr);
+#endif//DEBUG_LINKED_ITEM_CHAIN
+        existing_item->insert(new_item);
+    }
+    
+    return new_item;
+}
+
+
+/** @brief Remove one item from the list. 
+ 
+    Parameters first_ptr and last_ptr are pointers to the first and the last item of the list respectively.
+    They are updated as needed.
+ 
+    The item must be a valid object.
+    It must belong to the list.
+    
+    The list must not be empty.
+ */
+template<typename T> Item* remove_from_list(T* &first_ptr, T* &last_ptr, T* item)
+{
+#ifdef DEBUG_LINKED_ITEM_CHAIN
+    assert(item != nullptr);
+    assert(first_ptr != nullptr);
+    assert(last_ptr != nullptr);
+#endif//DEBUG_LINKED_ITEM_CHAIN
+    
+    if(first_ptr == item)
+    {
+        first_ptr = item->next();
+    }
+    
+    if(last_ptr == item)
+    {
+        last_ptr = item->prev();
+    }
+    
+    item->remove();
+    
+    return item;
+}
+
+
+/** @brief Remove all items from the list. 
+ 
+    Parameters first_ptr and last_ptr are set to null.
+ */
+template<typename T> void clear_list(T* &first_ptr, T* &last_ptr)
+{
+    while(last_ptr)
+        remove_from_list(first_ptr, last_ptr, last_ptr);
+}
+
+
+template<typename T> bool list_is_empty(T* first_ptr, T* last_ptr)
+{
+    return first_ptr == nullptr && last_ptr == nullptr;
+}
+
+
+/** @brief Container for linked items. */
+template<typename T> class LinkedItemChain{
+    T* first_item = nullptr; 
+    T* last_item = nullptr;
+    
+public:
+    bool isEmpty() const { return list_is_empty(first_item, last_item); }
+    
+    T* first() const { return first_item; }
+    
+    T* last() const { return last_item; }
+    
+    LinkedItemPointer<T> begin() const { return first_item; }
+    
+    LinkedItemPointer<T> end() const { return last_item ? last_item->next() : nullptr; }
+    
+    T* append(T* item) { return append_to_list(first_item, last_item, item); }
+    
+    T* append(LinkedItemChain* other) 
+    {
+        assert(this != other);
+        append(other->first());
+    }
+    
+    void clear() { return clear_list(first_item, last_item); }
+};
 
     
 }//namespace r64fx
