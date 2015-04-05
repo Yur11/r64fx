50b725984fd20e9ca12e5509c61f425e84695d55
Yuri Klopovsky <yuri.klopovsky@gmail.com>
2015-04-05 21:08:05 +0300

    Playing around with new stuff in main.cpp.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
+#include <iostream>
+
 #include "Window_SDL2.hpp"
+#include "Image.hpp"
+
+using namespace std;
+using namespace r64fx;
 
 int main(int argc, char* argv[])
 {
+    Image img(64, 64, 3);
+
+    cout << ( img.isGood() ) << "\n";
+
     return 0;
 }
