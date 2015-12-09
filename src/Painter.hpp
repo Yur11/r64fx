02a3910b046eb8a157779f2426c92464577d7adc
Yuri Klopovsky <yuri.klopovsky@gmail.com>
2015-12-09 03:07:54 +0200

    Added some doxygen comments to Painter.

--- src/Painter.hpp
+++ src/Painter.hpp
@@ -23,19 +23,52 @@ public:
 
     inline void setOffset(Point<int> offset) { m_offset = offset; }
 
+    /** @brief Set current offset value.
+        Positive x,y values denote an offset
+        towards the bottom right corner of the window.
+     */
     inline Point<int> offset() const { return m_offset; }
 
+    /** @brief Set current clipping rect.
+
+        NOTE The clipping rect postion is always set in
+        the coordinate system of the window.
+        The offset is not applied to it.
+     */
     virtual void setClipRect(Rect<int> rect) = 0;
 
+    /** @brief        Fill a rectangle with the given color.
+
+        @param color  Color to use.
+
+        @param rect   Rectangle coords. to fill.
+                      Offset and clipping are applied.
+                      to the rectange before it's used.
+     */
     virtual void fillRect(Color<unsigned char> color, Rect<int> rect) = 0;
 
+    /** @brief        Put an image at the given position.
+
+        @param image  Image to use. Must not be null.
+                      For now RGBA only!
+
+        @param pos    Position to put the image at.
+                      Top left corner.
+                      Offset and clipping are applied.
+     */
     virtual void putImage(Image* img, Point<int> pos = {0, 0}) = 0;
 
+    /** @brief Make the changes visible.
+
+        Update window surface. Swap buffers. etc.
+        Depending on the window type.
+     */
     virtual void repaint() = 0;
 
     /** @brief Adjust for the new window configuration.
 
-        Call this if the window has been resized.
+        Allways call this before any other paint commands
+        if the window has been resized.
      */
     virtual void reconfigure() = 0;
 };
