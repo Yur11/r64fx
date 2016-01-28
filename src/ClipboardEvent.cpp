b1182e9b9df9a9c334ca31667fecd80550350252
Yuri Klopovsky <yuri.klopovsky@gmail.com>
2016-01-28 02:26:04 +0200

    Not copying data in ClipboardDataTransmitEvent.

--- src/ClipboardEvent.cpp
+++ src/ClipboardEvent.cpp
@@ -1,16 +1,11 @@
 #include "ClipboardEvent.hpp"
-#include <cstring>
 
 namespace r64fx{
 
 void ClipboardDataTransmitEvent::transmit(void* data, int size)
 {
-    if(data != nullptr && size > 0)
-    {
-        m_data[0] = new char[size];
-        m_size[0] = size;
-        memcpy(m_data[0], data, size);
-    }
+    m_data[0] = data;
+    m_size[0] = size;
 }
 
 }//namespace r64fx
