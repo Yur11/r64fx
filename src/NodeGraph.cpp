61ef708e04daf3e13e0390e23970bec6abc8b952
Yuri Klopovsky <yuri.klopovsky@gmail.com>
2016-03-22 22:13:43 +0200

    Doing some FM synthesis.

--- src/NodeGraph.cpp
+++ src/NodeGraph.cpp
@@ -77,8 +77,11 @@ void* NodeGraphImpl::process()
         buffer[i] = 0.0f;
     }
 
-    float freq = 440.0f;
-    float t = 0.0f;
+    float freq1 = 440.0f;
+    float freq2 = 440.0f;
+    float t1 = 0.0f;
+    float t2 = 0.0f;
+    float coeff = 1.0f;
 
 
     bool running = true;
@@ -105,14 +108,57 @@ void* NodeGraphImpl::process()
 
         if(status)
         {
-            float d = freq / float(driver->sampleRate());
+            MidiEvent event;
+            while(midi_in->readEvents(&event, 1))
+            {
+                auto msg = event.message();
+                switch(msg.type())
+                {
+                    case MidiMessage::Type::ControlChange:
+                    {
+                        cout << msg.controllerNumber() << "," << msg.controllerValue() << "\n";
+
+                        if(msg.controllerNumber() == 1)
+                        {
+                            freq1 = 55.0f + 880.0f * float(msg.controllerValue()) / 128.0f;
+                        }
+
+                        if(msg.controllerNumber() == 2)
+                        {
+                            freq2 = 55.0f + 880.0f * float(msg.controllerValue()) / 128.0f;
+                        }
+
+                        if(msg.controllerNumber() == 3)
+                        {
+                            coeff = float(msg.controllerValue()) / 128.0f;
+                        }
+
+                        break;
+                    }
+
+                    default:
+                    {
+                        cout << "other\n";
+                        break;
+                    }
+                }
+            }
+
             for(int i=0; i<driver->bufferSize(); i++)
             {
-                buffer[i] = sin(t * M_PI * 2.0f);
-                t += d;
-                if(t >= 0.0f)
-                    t -= 1.0f;
+                float f = freq1;
 
+                float d2 = freq2 / float(driver->sampleRate());
+                f *= sin(t2 * M_PI * 2.0f) * coeff + (1.0f - coeff);
+                t2 += d2;
+                if(t2 >= 0.0f)
+                    t2 -= 1.0f;
+
+                float d1 = f / float(driver->sampleRate());
+                buffer[i] = sin(t1 * M_PI * 2.0f);
+                t1 += d1;
+                if(t1 >= 0.0f)
+                    t1 -= 1.0f;
             }
 
             audio_out->writeSamples(buffer, driver->bufferSize());
