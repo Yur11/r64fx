e6b384326499a0b1cfd2bd66cd883e7ddc1d90ce
Yuri Klopovsky <yuri.klopovsky@gmail.com>
2016-08-08 04:40:58 +0300

    SignalGraph. WIP.

--- src/SignalGraph.cpp
+++ src/SignalGraph.cpp
@@ -1,7 +1,95 @@
 #include "SignalGraph.hpp"
 
-
 namespace r64fx{
+    
+    
+void SignalConnection::prepare()
+{
+    
+}
+    
+    
+void SignalConnection::processSample(int i)
+{
+    m_sink[0] = m_source[0];
+}
 
+
+void SignalConnection::finish()
+{
+    
+}
+    
+
+void SignalGraph::prepare()
+{
+    for(auto item : m_single_nodes)
+    {
+        item->prepare();
+    }
+    
+    for(auto item : m_node_classes)
+    {
+        item->prepare();
+    }
+    
+    for(auto item : m_connections)
+    {
+        item->prepare();
+    }
+    
+    for(auto item : m_subgraphs)
+    {
+        item->prepare();
+    }
+}
+    
+    
+void SignalGraph::processSample(int i)
+{
+    for(auto item : m_single_nodes)
+    {
+        item->processSample(i);
+    }
+    
+    for(auto item : m_node_classes)
+    {
+        item->processSample(i);
+    }
+    
+    for(auto item : m_connections)
+    {
+        item->processSample(i);
+    }
+    
+    for(auto item : m_subgraphs)
+    {
+        item->processSample(i);
+    }
+}
+
+
+void SignalGraph::finish()
+{
+    for(auto item : m_single_nodes)
+    {
+        item->finish();
+    }
+    
+    for(auto item : m_node_classes)
+    {
+        item->finish();
+    }
+    
+    for(auto item : m_connections)
+    {
+        item->finish();
+    }
+    
+    for(auto item : m_subgraphs)
+    {
+        item->finish();
+    }
+}
 
 }//namespace r64fx
--- src/SignalGraph.hpp
+++ src/SignalGraph.hpp
@@ -2,16 +2,95 @@
 #define R64FX_SIGNAL_GRAPH_HPP
 
 #include "LinkedList.hpp"
-#include "SignalNode.hpp"
-#include "SignalNodeConnection.hpp"
-#include "SoundDriver.hpp"
-#include "MidiDispatcher.hpp"
 
 namespace r64fx{
 
-
-class SignalGraph{
+/* Any item that can be processed inside a SignalGraph. */
+class SignalGraphProcessable : public LinkedList<SignalGraphProcessable>::Node{
+public:
+    /* Called at the beginning of a cycle. */
+    virtual void prepare() = 0;
     
+    /* Called for each sample during a cycle. */
+    virtual void processSample(int i) = 0;
+    
+    /* Called at the end of a cycle. */
+    virtual void finish() = 0;
+};
+
+
+class SignalPort{
+    float* m_addr = nullptr;
+    
+public:
+    SignalPort(float* addr) : m_addr(addr) {}
+    
+    SignalPort() {}
+    
+    inline float* addr() const { return m_addr; }
+    
+    inline float &operator[](int i) { return m_addr[i]; }
+    
+    inline float &operator*() { return m_addr[0]; }
+};
+
+
+class SignalSource : public SignalPort{
+public:
+    using SignalPort::SignalPort;
+};
+
+
+class SignalSink : public SignalSource{
+public:
+    using SignalPort::SignalPort;    
+};
+
+
+class SignalConnection : public SignalGraphProcessable{
+    SignalSource m_source;
+    SignalSink   m_sink;
+    
+public:
+    SignalConnection(SignalSource source, SignalSink sink)
+    : m_source(source)
+    , m_sink(sink)
+    {}
+    
+    inline SignalSource source() const { return m_source; }
+    
+    inline SignalSink sink() const { return m_sink; }
+    
+    virtual void prepare();
+    
+    virtual void processSample(int i);
+    
+    virtual void finish();
+};
+
+
+class SignalNode : public SignalGraphProcessable{
+    
+};
+
+
+class SignalNodeClass : public SignalGraphProcessable{
+    
+};
+
+
+class SignalGraph : public SignalGraphProcessable{
+    LinkedList<SignalGraphProcessable> m_single_nodes;
+    LinkedList<SignalGraphProcessable> m_node_classes;
+    LinkedList<SignalGraphProcessable> m_connections;
+    LinkedList<SignalGraphProcessable> m_subgraphs;
+    
+public:
+    virtual void prepare() = 0;
+    
+    virtual void processSample(int i) = 0;
+    
+    virtual void finish() = 0;
 };
 
 }//namespace r64fx
