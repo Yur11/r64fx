be335467bfeb78c777694d1531dc67badc6c515f
Yuri Klopovsky <yuri.klopovsky@gmail.com>
2016-09-04 00:41:04 +0300

    Complex. Using structs.

--- src/Complex.hpp
+++ src/Complex.hpp
@@ -8,16 +8,7 @@ namespace r64fx{
 template<typename T> class Complex;
 template<typename T> class Polar;
 
-template<typename T> T Re(const Complex<T>&);
-template<typename T> T Im(const Complex<T>&);
-
-template<typename T> T Magnitude(const Polar<T>&);
-template<typename T> T Phase(const Polar<T>&);
-
-template<typename T> class Complex{
-    friend T Re<T>(const Complex<T>&);
-    friend T Im<T>(const Complex<T>&);
-
+template<typename T> struct Complex{
     T re = T();
     T im = T();
 
@@ -29,49 +20,58 @@ public:
     Complex(const Polar<T> &polar);
 
     Complex<T> &operator=(const Polar<T> &polar);
+
+    inline Complex<T> conjugate() const
+    {
+        return Complex<T>(re, -im);
+    }
+
+    inline T magnitude() const
+    {
+        return sqrt(re*re + im*im);
+    }
+
+    inline T phase() const
+    {
+        return atan2(im, re);
+    }
 };
 
-template<typename T> inline T &Re(const Complex<T> &num)
-{
-    return num.re;
-}
-
-template<typename T> inline T &Im(const Complex<T> &num)
-{
-    return num.im;
-}
-
-template<typename T> inline Complex<T> Conjugate(const Complex<T> &num)
-{
-    return Complex<T>(Re(num), -Im(num));
-}
 
 template<typename T> Complex<T> operator+(const Complex<T> &a, const Complex<T> &b)
 {
-    return Complex<T>(Re(a) + Re(b), Im(a) + Im(b));
+    return Complex<T>(a.re + b.re, a.im + b.im);
 }
 
 template<typename T> Complex<T> operator-(const Complex<T> &a, const Complex<T> &b)
 {
-    return Complex<T>(Re(a) - Re(b), Im(a) - Im(b));
+    return Complex<T>(a.re - b.re, a.im - b.im);
 }
 
-template<typename T> Complex<T> &operator*(const Complex<T> &a, const Complex<T> &b)
+template<typename T> Complex<T> operator*(const Complex<T> &a, const Complex<T> &b)
 {
-    return Complex<T>(Re(a)*Re(b) - Im(a)*Im(b), Im(a)*Re(b) + Re(a)*Im(b));
+    return Complex<T>(a.re*b.re - a.im*b.im, a.im*b.re + a.re*b.im);
+}
+
+template<typename T> Complex<T> operator/(const Complex<T> &a, const Complex<T> &b)
+{
+    return Complex<T>(
+        (a.re * b.re + a.im * b.im) / (b.re * b.re + b.im * b.im),
+        (a.im * b.re - a.re * b.im) / (b.re * b.re + b.im * b.im)
+    );
 }
 
 template<typename T> Complex<T> &operator+=(Complex<T> &a, const Complex<T> &b)
 {
-    Re(a) += Re(b);
-    Im(a) += Im(b);
+    a.re += b.re;
+    a.im += b.im;
     return a;
 }
 
 template<typename T> Complex<T> &operator-=(Complex<T> &a, const Complex<T> &b)
 {
-    Re(a) -= Re(b);
-    Im(a) -= Im(b);
+    a.re -= b.re;
+    a.im -= b.im;
     return a;
 }
 
@@ -81,15 +81,17 @@ template<typename T> Complex<T> &operator*=(Complex<T> &a, const Complex<T> &b)
     return a;
 }
 
+template<typename T> Complex<T> &operator/=(Complex<T> &a, const Complex<T> &b)
+{
+    a = a / b;
+    return a;
+}
 
-template<typename T> class Polar{
-    friend T Magnitude<T>(const Polar<T>&);
-    friend T Phase<T>(const Polar<T>&);
 
+template<typename T> struct Polar{
     T magnitude = T();
     T phase = T();
 
-public:
     Polar(){}
 
     Polar(const T &magnitude, const T &phase) : magnitude(magnitude), phase(phase) {}
@@ -101,9 +103,19 @@ public:
 
     Polar<T> &operator=(const Complex<T> &complex)
     {
-        magnitude = sqrt(Re(complex)*Re(complex) + Im(complex)*Im(complex));
-        phase = atan2(Im(complex), Re(complex));
+        magnitude = complex.magnitude();
+        phase = complex.phase();
         return *this;
+    }
+
+    T re()
+    {
+        return magnitude * cos(phase);
+    }
+
+    T im()
+    {
+        return magnitude * sin(phase);
     }
 };
 
@@ -116,18 +128,8 @@ template<typename T> Complex<T>::Complex(const Polar<T> &polar)
 
 template<typename T> Complex<T> &Complex<T>::operator=(const Polar<T> &polar)
 {
-    re = Magnitude(polar) * cos(Phase(polar));
-    im = Magnitude(polar) * sin(Phase(polar));
-}
-
-template<typename T> T &Magnitude(const Polar<T> &num)
-{
-    return num.magnitude;
-}
-
-template<typename T> T &Phase(const Polar<T> &num)
-{
-    return num.phase;
+    re = polar.re();
+    im = polar.im();
 }
 
 }//namespace r64fx
