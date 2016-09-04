356197416f10e9fd8d0adccb63544e6127db2b6a
Yuri Klopovsky <yuri.klopovsky@gmail.com>
2016-09-04 05:45:30 +0300

    FilterView. Plotting freq. responses.

--- src/FilterView.cpp
+++ src/FilterView.cpp
@@ -76,6 +76,7 @@ void cleanup()
 }
 
 class PoleZeroPlot;
+class ResponsePlot;
 
 }//namespace
 
@@ -84,6 +85,7 @@ struct FilterViewPrivate{
     FilterViewControllerIface* ctrl = nullptr;
 
     PoleZeroPlot* pole_zero_plot = nullptr;
+    ResponsePlot* response_plot  = nullptr;
 
     FilterViewPrivate()
     {
@@ -94,6 +96,8 @@ struct FilterViewPrivate{
     {
         cleanup();
     }
+
+    void updatePlots();
 };
 
 
@@ -107,6 +111,8 @@ class PoleZeroPlot : public Widget{
 
     vector<Complex<float>> zeros;
     vector<Complex<float>> poles;
+    Complex<float>* current_point = nullptr;
+    Point<int> handle_anchor = {0, 0};
 
 public:
     PoleZeroPlot(FilterViewPrivate* m, Widget* parent = nullptr)
@@ -115,65 +121,15 @@ public:
     {
         zeros.push_back({+1.0f, 0.0f});
         zeros.push_back({+1.0f, 0.0f});
-        zeros.push_back({-1.0f, 0.0f});
-        zeros.push_back({-1.0f, 0.0f});
-        poles.push_back({0.0f, +0.7f});
-        poles.push_back({0.0, 0.0});
-        poles.push_back({0.0, 0.0});
-        poles.push_back({0.0f, -0.7f});
-
+        zeros.push_back({+1.0f, 0.0f});
+//         poles.push_back({0.0f, +0.7f});
+//         poles.push_back({0.0f, +0.7f});
+//         poles.push_back({0.0f, +0.7f});
     }
 
     virtual ~PoleZeroPlot()
     {
 
-    }
-
-    virtual void paintEvent(PaintEvent* event)
-    {
-        auto p = event->painter();
-
-        p->fillRect({0, 0, width(), height()}, Color(0, 0, 0, 0));
-        if(zimg.isGood())
-        {
-            p->blendColors({0, 0}, Colors(Color(255, 255, 255, 0)), &zimg);
-        }
-
-        if(markup_img.isGood())
-        {
-            p->blendColors({0, 0}, Colors(Color(0, 127, 0, 0)), &markup_img);
-        }
-
-        Point<int> handle_offset((handle_size >> 1) + 1, (handle_size >> 1) + 1);
-
-        for(auto zero : zeros)
-        {
-            p->blendColors(
-                complexToPoint(zero) - handle_offset,
-                Colors(Color(0, 0, 255, 0)),
-                g_circle_img
-            );
-        }
-
-        for(auto pole : poles)
-        {
-            p->blendColors(
-                complexToPoint(pole) - handle_offset,
-                Colors(Color(255, 0, 0, 0)),
-                g_cross_img
-            );
-        }
-    }
-
-    virtual void resizeEvent(ResizeEvent* event)
-    {
-        updateImg();
-    }
-
-    virtual void mousePressEvent(MousePressEvent* event)
-    {
-        auto val = evaluateAt(pointToComplex(event->position()));
-        cout << val.re << ", " << val.im << " -> " << val.magnitude() << ", " << (val.phase() / M_PI) << " --> " << (int)zimg(event->x(), event->y())[0] << "\n";
     }
 
     Complex<float> evaluateAt(Complex<float> z)
@@ -182,12 +138,20 @@ public:
         for(auto zero : zeros)
         {
             numerator *= (z - zero);
+            if(zero.im != 0.0f)
+            {
+                numerator *= (z - zero.conjugate());
+            }
         }
 
         Complex<float> denominator(1, 0);
         for(auto pole : poles)
         {
             denominator *= (z - pole);
+            if(pole.im != 0.0f)
+            {
+                denominator *= (z - pole.conjugate());
+            }
         }
 
         if(denominator.magnitude() != 0)
@@ -196,33 +160,7 @@ public:
             return denominator;
     }
 
-    Complex<float> pointToComplex(Point<int> point) const
-    {
-        return {
-            +float((point.x() - width()/2)  / apparentRadius()),
-            -float((point.y() - height()/2) / apparentRadius())
-        };
-    }
-
-    Point<int> complexToPoint(Complex<float> complex) const
-    {
-        return{
-            +int(complex.re * apparentRadius()) + width()/2,
-            -int(complex.im * apparentRadius()) + height()/2
-        };
-    }
-
-    float apparentRadius() const
-    {
-        return widthOrHeight() * 0.45f;
-    }
-
-    int widthOrHeight() const
-    {
-        return min(width(), height());
-    }
-
-    void updateImg()
+    void update()
     {
         zimg.load(width(), height(), 1);
 
@@ -267,9 +205,210 @@ public:
             delete textimg;
         }
     }
+
+private:
+    virtual void paintEvent(PaintEvent* event)
+    {
+        auto p = event->painter();
+
+        p->fillRect({0, 0, width(), height()}, Color(0, 0, 0, 0));
+        if(zimg.isGood())
+        {
+            p->blendColors({0, 0}, Colors(Color(255, 255, 255, 0)), &zimg);
+        }
+
+        if(markup_img.isGood())
+        {
+            p->blendColors({0, 0}, Colors(Color(0, 127, 0, 0)), &markup_img);
+        }
+
+        Point<int> handle_offset((handle_size >> 1) + 1, (handle_size >> 1) + 1);
+
+        for(auto zero : zeros)
+        {
+            p->blendColors(
+                complexToPoint(zero) - handle_offset,
+                Colors(Color(0, 0, 255, 0)),
+                g_circle_img
+            );
+        }
+
+        for(auto pole : poles)
+        {
+            p->blendColors(
+                complexToPoint(pole) - handle_offset,
+                Colors(Color(255, 0, 0, 0)),
+                g_cross_img
+            );
+        }
+    }
+
+    virtual void resizeEvent(ResizeEvent* event)
+    {
+        update();
+        repaint();
+    }
+
+    virtual void mousePressEvent(MousePressEvent* event)
+    {
+        auto val = evaluateAt(pointToComplex(event->position()));
+        cout << val.re << ", " << val.im << " -> " << val.magnitude() << ", " << (val.phase() / M_PI) << " --> " << (int)zimg(event->x(), event->y())[0] << "\n";
+        Point<int> anchor(0, 0);
+        auto point = pointAt(event->position(), &anchor);
+        if(point)
+        {
+            current_point = point;
+            handle_anchor = anchor;
+        }
+    }
+
+    virtual void mouseReleaseEvent(MouseReleaseEvent* event)
+    {
+        current_point = nullptr;
+        handle_anchor = {0, 0};
+    }
+
+    virtual void mouseMoveEvent(MouseMoveEvent* event)
+    {
+        if(current_point)
+        {
+            auto pos = event->position() - handle_anchor;
+            current_point[0] = pointToComplex(pos);
+            m->updatePlots();
+            parent()->repaint();
+        }
+    }
+
+    Complex<float> pointToComplex(Point<int> point) const
+    {
+        return {
+            +float((point.x() - width()/2)  / apparentRadius()),
+            -float((point.y() - height()/2) / apparentRadius())
+        };
+    }
+
+    Point<int> complexToPoint(Complex<float> complex) const
+    {
+        return{
+            +int(complex.re * apparentRadius()) + width()/2,
+            -int(complex.im * apparentRadius()) + height()/2
+        };
+    }
+
+    float apparentRadius() const
+    {
+        return widthOrHeight() * 0.45f;
+    }
+
+    int widthOrHeight() const
+    {
+        return min(width(), height());
+    }
+
+    Complex<float>* pointAt(Point<int> pos, Point<int>* anchor_out = nullptr)
+    {
+        Complex<float>* result = nullptr;
+        Point<int> anchor(0, 0);
+
+        for(auto &pole : poles)
+        {
+            Point<int> pole_pos = complexToPoint(pole);
+            Point<int> d = (pos - pole_pos);
+
+            if(abs(d.x()) < handle_size/2 && abs(d.y()) < handle_size/2)
+            {
+                result = &pole;
+                anchor = {d.x(), d.y()};
+            }
+        }
+
+        for(auto &zero : zeros)
+        {
+            Point<int> zero_pos = complexToPoint(zero);
+            Point<int> d = (pos - zero_pos);
+
+            if(abs(d.x()) < handle_size/2 && abs(d.y()) < handle_size/2)
+            {
+                result = &zero;
+                anchor = {d.x(), d.y()};
+            }
+        }
+
+        if(anchor_out)
+        {
+            anchor_out[0] = anchor + Point<int>(handle_size/2, handle_size/2);
+        }
+        return result;
+    }
+};
+
+
+class ResponsePlot : public Widget{
+    FilterViewPrivate* m = nullptr;
+
+    vector<float> response;
+
+    Image img;
+
+public:
+    ResponsePlot(FilterViewPrivate* m, Widget* parent = nullptr)
+    : Widget(parent)
+    , m(m)
+    {
+
+    }
+
+    ~ResponsePlot()
+    {
+
+    }
+
+    void update()
+    {
+        if(width() > 0)
+        {
+            response.resize(width());
+            float rcp = 1.0f / float(response.size() - 1);
+            for(unsigned int i=0; i<response.size(); i++)
+            {
+                float phase = (float(i) * rcp) * M_PI;
+                Complex<float> z(Polar<float>(1.0, phase));
+                Complex<float> value = m->pole_zero_plot->evaluateAt(z);
+                response[i] = value.magnitude();
+            }
+
+            img.load(width(), height(), 1);
+            fill(&img, Color(0));
+            stroke_plot(&img, Color(255), {0, 0, img.width(), img.height()}, response.data(), 0.2f);
+        }
+    }
+
+private:
+    virtual void paintEvent(PaintEvent* event)
+    {
+        auto p = event->painter();
+        p->fillRect({0, 0, width(), height()}, Color(0, 0, 0, 0));
+        if(img.isGood())
+        {
+            p->blendColors({0, 0}, Colors(Color(255, 63, 0, 0)), &img);
+        }
+    }
+
+    virtual void resizeEvent(ResizeEvent* event)
+    {
+        update();
+        repaint();
+    }
 };
 
 }//namespace
+
+
+void FilterViewPrivate::updatePlots()
+{
+    pole_zero_plot->update();
+    response_plot->update();
+}
 
 
 FilterView::FilterView(FilterViewControllerIface* ctrl, Widget* parent)
@@ -279,8 +418,9 @@ FilterView::FilterView(FilterViewControllerIface* ctrl, Widget* parent)
     m->ctrl = ctrl;
 
     m->pole_zero_plot = new PoleZeroPlot(m, this);
+    m->response_plot = new ResponsePlot(m, this);
 
-    setSize({640, 480});
+    setSize({800, 400});
 }
 
 
@@ -292,6 +432,12 @@ FilterView::~FilterView()
         {
             delete m->pole_zero_plot;
         }
+
+        if(m->response_plot)
+        {
+            delete m->response_plot;
+        }
+
         delete m;
     }
 }
@@ -299,7 +445,14 @@ FilterView::~FilterView()
 
 void FilterView::resizeEvent(ResizeEvent* event)
 {
-    m->pole_zero_plot->setSize(event->size());
+    if(event->width() > event->height())
+    {
+        m->pole_zero_plot->setPosition({0, 0});
+        m->pole_zero_plot->setSize({event->height(), event->height()});
+
+        m->response_plot->setPosition({m->pole_zero_plot->width(), 0});
+        m->response_plot->setSize({event->width() - m->pole_zero_plot->width(), event->height()});
+    }
 }
 
 }//namespace r64fx
