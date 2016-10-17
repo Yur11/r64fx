2439b476f7a04230040205dc341e3d1fddbc3a36
Yuri Klopovsky <yuri.klopovsky@gmail.com>
2016-10-18 00:07:17 +0300

    Added SystemUtils.

---
+++ src/SystemUtils.hpp
@@ -0,0 +1,14 @@
+#ifndef R64fX_SYSTEM_UTILS_HPP
+#define R64fX_SYSTEM_UTILS_HPP
+
+#include <string>
+
+namespace r64fx{
+
+std::string get_env(const std::string &var);
+    
+std::string home_dir();
+    
+}//namespace r64fx
+
+#endif//R64fX_SYSTEM_UTILS_HPP
