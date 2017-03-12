0e019dfce27afba9ebb44e7a04ab3c56da32a1fb
Yuri Klopovsky <yuri.klopovsky@gmail.com>
2017-03-12 23:22:18 +0200

    Working on Knobs.

--- src/View_Project.cpp
+++ src/View_Project.cpp
@@ -9,12 +9,6 @@ using namespace std;
 
 namespace r64fx{
 
-enum class KnobDecoration{
-    SolidHorseShoe,
-    DashedHorseShoe
-};
-
-
 class KnobAnimGenerator{
     int m_size;
     Image marker;
@@ -35,7 +29,7 @@ public:
         }
     }
 
-    void genKnob(Image* dst, Point<int> dstpos, KnobDecoration kd)
+    void genKnob(Image* dst, Point<int> dstpos)
     {
 #ifdef R64FX_DEBUG
         assert(dst->componentCount() == 2);
@@ -46,23 +40,8 @@ public:
         Image alpha_mask(m_size, m_size, 1);
         fill(&alpha_mask, 0, 1, 255);
 
-        switch(kd)
-        {
-            case KnobDecoration::SolidHorseShoe:
-            {
-                genDecorationSolidHorseShoe(&alpha_mask, dstpos);
-                break;
-            }
-
-            case KnobDecoration::DashedHorseShoe:
-            {
-                genDecorationDashedHorseShoe(&alpha_mask, dstpos, 10);
-                break;
-            }
-
-            default:
-                break;
-        }
+//         genDecorationSolidHorseShoe(&alpha_mask);
+        genDecorationSegmentedRing(&alpha_mask, 8);
 
         genKnobCenter(dst, &alpha_mask, dstpos);
         copy(dst, dstpos, &alpha_mask, ChanShuf(1, 1, 0, 1));
@@ -122,25 +101,36 @@ private:
         }
     }
 
-    void genDecorationSolidHorseShoe(Image* alpha_mask, Point<int> dstpos)
+    void genBaseRing(Image* ring)
+    {
+        fill(
+            ring, 0, 1, 255
+        );
+        fill_circle(
+            ring, 0, 1, Color(0), {0, 0}, m_size
+        );
+        fill_circle(
+            ring, 0, 1, Color(255), {3, 3}, m_size - 6
+        );
+    }
+
+    void genBaseHorseShoe(Image* horse_shoe)
+    {
+        genBaseRing(horse_shoe);
+        fill_bottom_triangle(
+            horse_shoe, 1, 1, Color(255), {0, 0}, m_size
+        );
+    }
+
+    void genDecorationSolidHorseShoe(Image* alpha_mask)
     {
         Image horse_shoe(m_size, m_size, 1);
-        fill(&horse_shoe, 0, 1, 255);
-
-        fill_circle(
-            &horse_shoe, 0, 1, Color(0), {0, 0}, m_size
-        );
-        fill_circle(
-            &horse_shoe, 0, 1, Color(255), {3, 3}, m_size - 6
-        );
-        fill_bottom_triangle(
-            &horse_shoe, 1, 1, Color(255), {0, 0}, m_size
-        );
+        genBaseHorseShoe(&horse_shoe);
 
         Image tick(m_size, m_size, 1);
         fill(&tick, 0, 1, 255);
         fill({&tick, {m_size/2 - 1, 2, 3, 10}}, 0, 1, 0);
-        copyTick(&horse_shoe, &tick, -M_PI * 0.75f);
+        copyTick(&horse_shoe, &tick, -M_PI * 0.75f, PixOpMin());
 
         mirror_left2right(&horse_shoe);
         copy(alpha_mask, {0, 0}, &horse_shoe, PixOpMin());
@@ -149,41 +139,33 @@ private:
         fill({alpha_mask, {halfSize() - 1, 0, 2, 5}}, 0, 1, 0);
     }
 
-    void genDecorationDashedHorseShoe(Image* alpha_mask, Point<int> dstpos, int nticks)
+    void genDecorationSegmentedRing(Image* alpha_mask, int nsegments)
     {
-        Image horse_shoe(m_size, m_size, 1);
-        fill(&horse_shoe, 0, 1, 255);
+        Image ring(m_size, m_size, 1);
+        genBaseRing(&ring);
 
-        Image long_tick(m_size, m_size, 1);
-        fill(&long_tick, 0, 1, 255);
-        fill({&long_tick, {m_size/2 - 1, 2, 2, 4}}, 0, 1, 0);
-
-        Image short_tick(m_size, m_size, 1);
-        fill(&short_tick, 0, 1, 255);
-        fill({&short_tick, {m_size/2 - 1, 2, 2, 4}}, 0, 1, 0);
-
-        float angle_range = M_PI * 0.75;
-        float angle = -angle_range;
-        float angle_step = angle_range / float(nticks);
-
-        for(int i=0; i<nticks; i++)
+        Image tick(m_size, m_size, 1);
+        fill(&tick, 0, 1, 0);
+        fill({&tick, {m_size/2 - 1, 0, 2, 6}}, 0, 1, 255);
+        float angle_range = M_PI * 2.0f;
+        float angle_step = angle_range / float(nsegments);
+        float angle = -M_PI;
+        for(int i=0; i<nsegments; i++)
         {
-            copyTick(&horse_shoe, i > 0 ? &short_tick : &long_tick, angle);
+            copyTick(&ring, &tick, angle, PixOpAdd());
             angle += angle_step;
         }
-        copy(&horse_shoe, {0, 0}, &long_tick, PixOpMin());
 
-        mirror_left2right(&horse_shoe);
-        copy(alpha_mask, {0, 0}, &horse_shoe, PixOpMin());
+        copy(alpha_mask, {0, 0}, &ring, PixOpMin());
     }
 
-    void copyTick(Image* horse_shoe, Image* tick, float angle)
+    void copyTick(Image* horse_shoe, Image* tick, float angle, PixelOperation pixop)
     {
         Transformation2D<float> t;
         t.translate(+halfSize() - 0.5f, +halfSize() - 0.5f);
         t.rotate(angle);
         t.translate(-halfSize() + 0.5f, -halfSize() + 0.5f);
-        copy(horse_shoe, t, tick, PixOpMin());
+        copy(horse_shoe, t, tick, pixop);
     }
 
 public:
@@ -239,43 +221,13 @@ void View_Project::paintEvent(WidgetPaintEvent* event)
         KnobAnimGenerator kanimg(size);
 
         Image knob(size, size, 2);
-        kanimg.genKnob(&knob, {0, 0}, (i & 1) ? KnobDecoration::SolidHorseShoe : KnobDecoration::DashedHorseShoe);
+        kanimg.genKnob(&knob, {0, 0});
         p->putImage(&knob, {10 + (i * size + 20), 20});
 
         Image marker(size, size, 2);
         kanimg.genMarker(&marker, {0, 0}, m_angle);
         p->putImage(&marker, {10 + (i * size + 20), 20});
     }
-
-    Image gradimg(50, 50, 1);
-    fill(&gradimg, Color(0));
-    fill_gradient_radial(&gradimg, 0, 1, 0, 255, {0, 0, 50, 50});
-
-    Image circleimg(50, 50, 1);
-    fill(&circleimg, Color(0));
-    fill_circle(&circleimg, 0, 1, Color(255), {0, 0}, 50);
-    copy(&gradimg, {0, 0}, &circleimg, PixOpMul());
-
-    Image img(50, 50, 4);
-    fill(&img, Color(0, 0, 0, 0));
-
-    threshold(&img, 0, 4, {0, 0}, Color(255, 0, 0, 0), Color(255, 0, 0, 255), &gradimg, 255.0f * ((normalize_angle(m_angle - M_PI * 0.5f) / (2.0f * M_PI))));
-    flip_hori(&img);
-    p->putImage(&img, {100, 100});
-
-    Image tick(50, 50, 4);
-    fill(&tick, Color(255, 0, 0, 255));
-    fill({&tick, {23, 0, 4, 27}}, Color(255, 0, 0, 0));
-    Image tickimg(50, 50, 4);
-    fill(&tickimg, Color(0, 0, 0, 255));
-    Transformation2D<float> t;
-    t.translate(float(25) - 0.5f, float(25) - 0.5f);
-    t.rotate(-(M_PI * 0.5f + m_angle));
-    t.translate(float(-25) + 0.5f, float(-25) + 0.5f);
-    copy(&tickimg, t, &tick, PixOpReplace());
-
-    p->putImage(&tickimg, {100, 100});
-
 }
 
 
