8ac66b15d9facc566fc0c690465b3d8d47cd5348
Yuri Klopovsky <yuri.klopovsky@gmail.com>
2017-07-09 07:31:03 +0300

    Getting changing values in sliders & knobs.

--- src/View_Project.cpp
+++ src/View_Project.cpp
@@ -16,6 +16,11 @@ namespace r64fx{
 
 Font* g_font = nullptr;
 
+void value_changed(void* arg, float new_value)
+{
+    cout << arg << ": " << new_value << "\n";
+}
+
 View_Project::View_Project(Widget* parent) : Widget(parent)
 {
     if(!g_font)
@@ -23,8 +28,15 @@ View_Project::View_Project(Widget* parent) : Widget(parent)
         g_font = new Font("mono", 14);
     }
 
-    auto knob = new Widget_Knob(48, this);
-    knob->setPosition({50, 50});
+    Widget_Knob* knob[4];
+    for(int i=0; i<4; i++)
+    {
+        knob[i] = new Widget_Knob(48, this);
+        knob[i]->setPosition({50 + (knob[i]->width() + 10) * i, 50});
+        knob[i]->onValueChanged(value_changed, knob[i]);
+    }
+    knob[0]->setMinValue(0.0f);
+    knob[1]->setMaxValue(0.0f);
 
     Widget_Slider* slider_vert[4];
     for(int i=0; i<4; i++)
@@ -33,6 +45,7 @@ View_Project::View_Project(Widget* parent) : Widget(parent)
         slider_vert[i]->setPosition({50 + (slider_vert[i]->width() + 2) * i, 120});
         slider_vert[i]->setMinValue(-1.0f);
         slider_vert[i]->setMaxValue(+1.0f);
+        slider_vert[i]->onValueChanged(value_changed, slider_vert[i]);
     }
     slider_vert[0]->setMinValue(0.0f);
     slider_vert[1]->setMaxValue(0.0f);
@@ -44,6 +57,7 @@ View_Project::View_Project(Widget* parent) : Widget(parent)
         slider_hori[i]->setPosition({50, 350 + (slider_hori[i]->height() + 2) * i});
         slider_hori[i]->setMinValue(-1.0f);
         slider_hori[i]->setMaxValue(+1.0f);
+        slider_hori[i]->onValueChanged(value_changed, slider_vert[i]);
     }
     slider_hori[0]->setMinValue(0.0f);
     slider_hori[1]->setMaxValue(0.0f);
