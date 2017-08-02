b4424947b3df4803edb5c73e8af73db30ae9b9ae
Yuri Klopovsky <yuri.klopovsky@gmail.com>
2017-08-02 08:01:21 +0300

    Can run prepare_shaders.sh from any directory.

--- tools/prepare_shaders.sh
+++ tools/prepare_shaders.sh
@@ -7,10 +7,15 @@ then
     g++ -std=c++11 -O0 ../tools/makestring.cpp -o makestring
 fi
 
+MAKESTRINGCMD="`pwd`/makestring"
+echo $MAKESTRINGCMD
+
+if [ "$#" != "0" ]; then cd $1; fi
 for i in `find . -name "*.vert" -o -name "*.frag"`
 do
-    ./makestring < $i > $i.h
+    `echo $MAKESTRINGCMD` < $i > $i.h
     echo "$i.h"
 done
+if [ "$#" != "0" ]; then cd -; fi
 
 rm ./makestring
