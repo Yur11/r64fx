a2808bc5760eeafbce1d7868d2c364679c73fd80
Yuri Klopovsky <yuri.klopovsky@gmail.com>
2017-09-03 21:37:21 +0300

    Working on ModuleConnection.

--- src/ModuleConnection.cxx
+++ src/ModuleConnection.cxx
@@ -6,8 +6,21 @@ namespace{
 
 R64FX_DECL_DEFAULT_MODULE_AGENTS(ModuleConnectionManager);
 
-class ModuleConnectionManagerThreadObjectImpl : public ModuleThreadObjectImpl{
 
+struct ModuleConnectionMessage{
+    SignalSource* source         = nullptr;
+    SignalSink*   sink           = nullptr;
+    int           source_offset  = 0;
+    int           sink_offset    = 0;
+    int           size           = 0;
+    int           type           = 0;
+    void*         handle         = 0;
+};
+
+
+/*======= Worker Thread =======*/
+
+class ModuleConnectionManagerThreadObjectImpl : public ModuleThreadObjectImpl{
 public:
     ModuleConnectionManagerThreadObjectImpl(ModuleConnectionManagerDeploymentAgent* agent, R64FX_DEF_THREAD_OBJECT_IMPL_ARGS)
     : ModuleThreadObjectImpl(agent, R64FX_THREAD_OBJECT_IMPL_ARGS)
@@ -26,15 +39,23 @@ public:
 private:
     virtual void messageFromIfaceRecieved(const ThreadObjectMessage &msg)
     {
-        switch(msg.key())
+        auto* msgs = (ModuleConnectionMessage*) msg.value();
+        long nmsgs = msg.key();
+#ifdef R64FX_DEBUG
+        assert(msgs);
+        assert(nmsgs > 0);
+#endif//R64FX_DEBUG
+
+        for(int i=0; i<nmsgs; i++)
         {
-            default:
-            {
-                abort(); //Must never happen!
-            }
+            auto &message = msgs[i];
+#ifdef R64FX_DEBUG
+            assert(message.source);
+            assert(message.sink);
+#endif//R64FX_DEBUG
         }
 
-        sendMessagesToIface(&msg, 1); //Always respond so that iface can free the message payload.
+        sendMessagesToIface(&msg, 1);
     }
 
     inline void prologue()
@@ -62,13 +83,12 @@ public:
 private:
     virtual void messageFromImplRecieved(const ThreadObjectMessage &msg) override final
     {
-        switch(msg.key())
-        {
-            default:
-            {
-                abort();
-            }
-        }
+        auto msgs = (ModuleConnectionMessage*) msg.value();
+        long nmsgs = msg.key();
+#ifdef R64FX_DEBUG
+        assert(msgs);
+        assert(nmsgs > 0);
+#endif//R64FX_DEBUG
     }
 };
 
