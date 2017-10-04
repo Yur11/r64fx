1d9b48d26bdefab130e1ac1d2c42f4d83a291040
Yuri Klopovsky <yuri.klopovsky@gmail.com>
2017-10-04 23:47:53 +0300

    Added Debug.hpp

---
+++ src/Debug.hpp
@@ -0,0 +1,10 @@
+#ifndef R64FX_DEBUG_HPP
+#define R64FX_DEBUG_HPP
+
+#ifdef R64FX_DEBUG
+#define R64FX_DEBUG_ASSERT(arg) assert(arg)
+#else
+#define R64FX_DEBUG_ASSERT(arg)
+#endif//R64FX_DEBUG
+
+#endif//R64FX_DEBUG_HPPs
