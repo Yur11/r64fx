b08650fa4ee0ef4cc6784a6f05a74a9b63328ff5
Yuri Klopovsky <yuri.klopovsky@gmail.com>
2018-01-09 11:21:19 +0200

    Fixed some debug macros.

--- src/Debug.hpp
+++ src/Debug.hpp
@@ -7,9 +7,14 @@
 #endif//R64FX_DEBUG
 
 #ifdef R64FX_DEBUG
-#define R64FX_DEBUG_ASSERT(arg) assert(arg)
+#define R64FX_DEBUG_ASSERT(arg) { assert(arg); }
+#define R64FX_DEBUG_ABORT(msg) { std::cerr << msg; abort(); }
+
 #else
 #define R64FX_DEBUG_ASSERT(arg)
+#define R64FX_DEBUG_ABORT(msg)
+
 #endif//R64FX_DEBUG
+
 
 #endif//R64FX_DEBUG_HPPs
