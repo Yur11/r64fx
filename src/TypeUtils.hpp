bbdfc79701e40bf599540716688bbd92f001a3f8
Yuri Klopovsky <yuri.klopovsky@gmail.com>
2018-01-26 01:35:20 +0200

    TypeUtils: Added R64FX_OPAQUE_HANDLE(T) macro.

--- src/TypeUtils.hpp
+++ src/TypeUtils.hpp
@@ -1,12 +1,8 @@
 #ifndef R64FX_TYPE_UTILS_HPP
 #define R64FX_TYPE_UTILS_HPP
 
-#define R64FX_VALUE_TYPE(name, type)\
-    class name{ type m_value = {}; public: explicit name(type value) : m_value(value) {};\
-        inline type value() const{ return m_value; }\
-        inline bool operator==(const name &other) { return value() == other.value(); }\
-        inline bool operator!=(const name &other) { return value() != other.value(); }\
-        inline explicit operator type() { return value(); }\
-    }
+/* Prevent class instances from being constructed, copied or deleted. */
+#define R64FX_OPAQUE_HANDLE(T)\
+private: T(){} T(const T&){} inline void operator=(const T&){} ~T(){}
 
 #endif//R64FX_TYPE_UTILS_HPP
