58ac4c7d89140e67ad3b02231c3f6e920bf38870
Yuri Klopovsky <yuri.klopovsky@gmail.com>
2018-09-02 12:34:06 +0300

    Declared jit_procedures module

---
+++ src/jit_procedures.hpp
@@ -0,0 +1,12 @@
+#ifndef R64FX_JIT_PROCEDURES_HPP
+#define R64FX_JIT_PROCEDURES_HPP
+
+#include "jit.hpp"
+
+namespace r64fx{
+
+    
+
+}//namespace r64fx
+
+#endif//R64FX_JIT_PROCEDURES_HPP
---
+++ src/test_jit_procedures.cpp
@@ -0,0 +1,11 @@
+#include "test.hpp"
+#include "jit_procedures.hpp"
+
+using namespace std;
+using namespace r64fx;
+
+int main(int argc, char** argv)
+{
+
+    return 0;
+}
