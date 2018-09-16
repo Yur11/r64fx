f69aa8e51eff6f1862721eec7e2395390356bc35
Yuri Klopovsky <yuri.klopovsky@gmail.com>
2018-09-16 22:51:37 +0300

    jit_procedures: Doing basic looping on a buffer

--- src/jit_procedures.hpp
+++ src/jit_procedures.hpp
@@ -43,15 +43,16 @@
 #define R64FX_LOAD_JIT_PROC_ARGS(...)                                           \
     JitProcCommon::loadJitProcArgs(__VA_ARGS__, rbx)
 
-/* Load loop entry addr into rax.
- * Increment rcx. It is negative and grows towards zero.
- * Load next proc addr into rax if rcx reached zero.
+/* Load loop entry addr into PTR.
+ * Increment CNT. It is negative and grows towards zero.
+ * Load next proc addr into PTR if CNT reached zero.
  */
-#define R64FX_JIT_PROC_LOOP_BY(COUNT)                                           \
-    leaNextInstruction(rax);                                                    \
-    ADD    (rcx, Imm8(COUNT));                                                  \
-    CMOVZ  (rax, rbx);
+#define R64FX_JIT_PROC_LOOP(PTR, CNT, N)                                        \
+    leaNextInstruction(PTR);                                                    \
+    ADD    (CNT, Imm8(N));                                                      \
+    CMOVZ  (PTR, rbx)
 
+#define R64FX_JIT_LABEL(NAME) JumpLabel NAME; mark(NAME)
 
 namespace r64fx{
 
@@ -93,11 +94,16 @@ public:
 
         PUSH (rbx);
         PUSH (rbp);
+        PUSH (rdi);
+        PUSH (rsi);
+        PUSH (r12);
+
+        XOR(rax, rax);
 
         JumpLabel main_loop_entry; mark(main_loop_entry);
-        MOV  (rax, Base(rdi));  //Load first proc addr.
+        MOV  (rdx, Base(rdi));  //Load first proc addr.
         XOR  (rbp, rbp);        //Reset JitProcSequence index
-        JMP  (rax);             //Jump to first proc.
+        JMP  (rdx);             //Jump to first proc.
 
         /* ------------------------------------------- */
 
@@ -105,6 +111,9 @@ public:
         ADD  (rsi, Imm8(1));
         JNZ  (main_loop_entry);
 
+        POP  (r12);
+        POP  (rsi);
+        POP  (rdi);
         POP  (rbp);
         POP  (rbx);
         RET  ();
@@ -152,19 +161,64 @@ R64FX_DEF_JIT_PROC_PARAMS(Gain, float* src, float* coeffs, float* dst, long coun
 R64FX_DEF_JIT_PROC(Gain)
 {
     R64FX_LOAD_JIT_PROC_ARGS(r8, r9, r10, rcx);
-    R64FX_JIT_PROC_LOOP_BY(4)
+    R64FX_JIT_PROC_LOOP(rdx, rcx, 4);
         MOVAPS (xmm0, Base(r8) + Index(rcx)*4);
         MULPS  (xmm0, Base(r9) + Index(rcx)*4);
         MOVAPS (Base(r10) + Index(rcx)*4, xmm0);
-    JMP(rax); //Next Iteration or Next Proc.
+    JMP(rdx); //Next Iteration or Next Proc.
 }
 
-    MOVAPS (xmm0, Base(r8) + Index(rcx)*4);
-    MULPS  (xmm0, Base(r9) + Index(rcx)*4);
 
-    MOVAPS (Base(rbx) + Index(rcx)*4, xmm0);
+R64FX_DEF_JIT_PROC_PARAMS(Playback, int* playheads, float* buffers, float* dst, long count)
+{
+    R64FX_DEBUG_ASSERT(playheads);
+    R64FX_DEBUG_ASSERT(buffers);
+    R64FX_DEBUG_ASSERT(count > 0);
+    R64FX_DEBUG_ASSERT((count & 3) == 0);
+    R64FX_SAVE_JIT_PROC_ARGS(playheads+count-4, buffers, dst+count-4, -count);
+}
 
-    JMP    (rax); //Next Iteration or Next Proc.
+R64FX_DEF_JIT_PROC(Playback)
+{
+    R64FX_LOAD_JIT_PROC_ARGS(r8, r9, r10, rcx);
+    R64FX_JIT_PROC_LOOP(rdx, rcx, 4);
+
+        /* Fetch Playhead */
+        MOVDQA (xmm0, Base(r8) + Index(rcx)*4);
+
+        /* Fetch Data */
+        XORPS  (xmm1, xmm1);
+
+        MOV(r11, Imm32(-4));
+        R64FX_JIT_LABEL(FetchData);
+            MOVD   (rax, xmm0);
+            SHUFPS (xmm0, xmm0, Shuf(1, 2, 3, 0));
+            MOVSS  (xmm2, Base(r9) + Index(rax)*4);
+            ORPS   (xmm1, xmm2);
+            SHUFPS (xmm1, xmm1, Shuf(1, 2, 3, 0));
+        ADD(r11, Imm8(1));
+        JNZ(FetchData);
+
+        /* Store Output */
+        MOVAPS  (Base(r10) + Index(rcx)*4, xmm1);
+
+        /* Move Playhead */
+        PADDD   (xmm0, Base(r8) + Index(rcx)*4 + Disp(16));
+
+        /* Loop Playhead */
+        MOVDQA  (xmm1, xmm0);
+        MOVDQA  (xmm2, Base(r8) + Index(rcx)*4 + Disp(32));
+        PCMPGTD (xmm1, xmm2);
+        MOVDQA  (xmm3, xmm1);
+        PAND    (xmm1, xmm2);
+        PAND    (xmm3, Base(r8) + Index(rcx)*4 + Disp(48));
+        PSUBD   (xmm0, xmm1);
+        PADDD   (xmm0, xmm3);
+
+        /* Store Playhead */
+        MOVDQA (Base(r8) + Index(rcx)*4, xmm0);
+
+    JMP(rdx);
 }
 
 }//namespace r64fx
--- src/test_jit_procedures.cpp
+++ src/test_jit_procedures.cpp
@@ -8,26 +8,55 @@ float a[4] = { 1.0, 2.0, 3.0, 4.0 };
 float b[4] = { 1.0, 0.2, 0.5, 2.0 };
 float c[4] = { 0.0, 0.0, 0.0, 0.0 };
 
-inline void dump(float v[4])
+inline void dump(float* v)
 {
     cout << v[0] << ", " << v[1] << ", " << v[2] << ", " << v[3] << "\n";
-}
+};
+
+int playheads[16] = {
+    /* value */  0,  9, 16, 26,
+    /* delta */  1,  1,  1,  1,
+    /* last  */  7, 15, 23, 31,
+    /* first */  -1,  7, 15, 23
+};
+
+float buff[32] = {
+    11, 1, 2, 3, 4, 5, 6, 7,
+    22, 1, 2, 3, 4, 5, 6, 7,
+    33, 1, 2, 3, 4, 5, 6, 7,
+    44, 1, 2, 3, 4, 5, 6, 7
+};
+
+float out[4] = { 0, 0, 0, 0 };
 
 int main(int argc, char** argv)
 {
-    JitProc_Gain<JitProcCommon> jit;
+//     JitProc_Gain<JitProcCommon> jit;
+// 
+//     JitProcSequence seq;
+//     add(seq, jit.proc_Gain(), a, b, c, 4);
+//     seq.store((long)jit.procSeqExit());
+// 
+//     dump(a);
+//     dump(b);
+//     dump(c);
+//     cout << jit.exec(seq, 256) << "\n";
+//     dump(a);
+//     dump(b);
+//     dump(c);
 
+    JitProc_Playback<JitProcCommon> jit;
     JitProcSequence seq;
-    add(seq, jit.proc_Gain(), a, b, c, 4);
+    add(seq, jit.proc_Playback(), playheads, buff, out, 4);
     seq.store((long)jit.procSeqExit());
 
-    dump(a);
-    dump(b);
-    dump(c);
-    cout << jit.exec(seq, 256) << "\n";
-    dump(a);
-    dump(b);
-    dump(c);
+    dump(out);
+    for(int i=0; i<16; i++)
+    {
+        jit.exec(seq, 1);
+        dump(out);
+    }
+
 
     return 0;
 }
