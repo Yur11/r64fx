8b4cd4c03d8b369ee42a03308f8d53fa776eed5e
Yuri Klopovsky <yuri.klopovsky@gmail.com>
2019-02-10 15:08:28 +0200

    Deleted jit_algorithms.hpp

--- src/test_Synth.cpp
+++ src/test_Synth.cpp
@@ -4,7 +4,6 @@
 #include "SoundDriver.hpp"
 #include "TimeUtils.hpp"
 #include "Thread.hpp"
-#include "jit_algorithms.hpp"
 
 #define CLIENT_NAME "test_Synth"
 #define SOUND_DRIVER_TYPE SoundDriver::Type::Jack
@@ -19,17 +18,9 @@ bool g_running = true;
 SoundDriverPortGroup* g_port_group = nullptr;
 SoundDriverAudioOutput *g_audio_output = nullptr;
 
-constexpr float  INT_RCP     = 1.0f / int(0x7FFFFFFF);
-constexpr float  PI_INT_RCP  = INT_RCP * 3.1415926535897932384626f;
-
-
 int   g_buffer_size      = 0.0f;
 float g_sample_rate      = 0.0f;
 float g_sample_rate_rcp  = 0.0f;
-
-JitAlgorithmConstants g_contants __attribute__((aligned(64)));
-
-SineOscBuffers g_buffers __attribute__((aligned(64)));
 
 
 inline void wait_sync()
@@ -50,53 +41,8 @@ inline void one_cycle()
 }
 
 
-inline float osc_cos(JitAlgorithmConstants* constants, SineOscBuffers* buffers)
-{
-    int osc = buffers->v0.osc[0];
-    float s = float(osc) * constants->v0.pi_int_rcp;
-
-    s = s*s;
-    float x = s;
-    float val = 1.0f;
-            val -= x * constants->v0.f2_rcp;
-    x *= s; val += x * constants->v0.f4_rcp;
-    x *= s; val -= x * constants->v1.f6_rcp;
-    x *= s; val += x * constants->v1.f8_rcp;
-    x *= s; val -= x * constants->v1.f10_rcp;
-
-    osc += buffers->v1.increment[0];
-    buffers->v0.osc[0] = osc;
-    return val;
-}
-
-
-class SynthAssembler : public Assembler{
-public:
-    SynthAssembler() { resize(0, 1); }
-
-    auto genOscCos()
-    {
-        auto fun_addr = codeEnd();
-        gen_sine_osc(this, rdi, rsi, xmm0, xmm1, xmm2, xmm3, xmm4);
-        RET();
-        return (float (*)(JitAlgorithmConstants*, SineOscBuffers*))fun_addr;
-    }
-
-    auto genFun()
-    {
-        auto fun_addr = codeEnd();
-        MOV(rax, rsi);
-        RET();
-        return (long(*)(long, long))fun_addr;
-    }
-};
-
-
 void* worker_thread(void* arg)
 {
-    SynthAssembler sa;
-    auto jit_osc_cos = sa.genOscCos();
-
     auto sd = SoundDriver::newInstance(SOUND_DRIVER_TYPE, CLIENT_NAME);
     sd->enable();
 
@@ -117,10 +63,10 @@ void* worker_thread(void* arg)
     g_sample_rate = sd->sampleRate();
     g_sample_rate_rcp = 1.0f / g_sample_rate;
 
-    float freq = 440.0f;
-    int increment = freq * g_sample_rate_rcp * 0xFFFFFFFFU;
-    for(int i=0; i<4; i++)
-        g_buffers.v1.increment[i] = increment;
+//     float freq = 440.0f;
+//     int increment = freq * g_sample_rate_rcp * 0xFFFFFFFFU;
+//     for(int i=0; i<4; i++)
+//         g_buffers.v1.increment[i] = increment;
 
     while(g_running)
     {
@@ -129,8 +75,7 @@ void* worker_thread(void* arg)
             for(int i=0; i<g_buffer_size; i++)
             {
 //                 float val = osc_cos(&g_contants, &g_buffers) * 0.5f;
-                float val = jit_osc_cos(&g_contants, &g_buffers) * 0.5f;
-                audio_buffer[i] = val;
+//                 audio_buffer[i] = val;
             }
             g_port_group->done();
         }
