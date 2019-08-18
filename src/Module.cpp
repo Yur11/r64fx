381ccd7e3f3ae66ff0673365009df20543908cb3
Yuri Klopovsky <yuri.klopovsky@gmail.com>
2019-08-18 20:59:10 +0300

    Changes in Module

---
+++ src/Module.cpp
@@ -0,0 +1,15 @@
+#include "Module.hpp"
+
+namespace r64fx{
+
+void Module::engage()
+{
+
+}
+
+void Module::disengage()
+{
+
+}
+
+}//namespace r64fx
--- src/Module.hpp
+++ src/Module.hpp
@@ -1,78 +1,20 @@
 #ifndef R64FX_MODULE_HPP
 #define R64FX_MODULE_HPP
 
-#include "TypeUtils.hpp"
+#include "LinkedList.hpp"
 
 namespace r64fx{
 
-class Module;
-class ModuleThreadHandle;
-
-
-class Module{
-    friend class ModulePrivate;
+class Module : public LinkedList<Module>::Node{
+    long                m_id = 0;
+    LinkedList<Module>  m_children;
 
 public:
-    Module();
+    inline long id() const { return m_id; }
 
-    virtual ~Module();
+    void engage();
 
-    typedef void (Callback)(Module* module, void* arg);
-
-    virtual bool engage(
-        Module::Callback* done = nullptr, void* done_arg = nullptr, ModuleThreadHandle* threads = nullptr, int nthreads = 0
-    ) = 0;
-
-    virtual void disengage(Module::Callback* done = nullptr, void* done_arg = nullptr) = 0;
-
-    virtual bool isEngaged() = 0;
-
-    virtual bool engagementPending() = 0;
-
-    static void genThreads(ModuleThreadHandle** threads, int nthreads);
-
-    static int freeThreads(ModuleThreadHandle** threads, int nthreads);
-};
-
-
-enum class ModulePortType : unsigned long{
-    SignalSource, SignalSink
-};
-
-class ModulePort{
-    R64FX_OPAQUE_HANDLE(ModulePort)
-
-public:
-    ModuleThreadHandle* thread();
-};
-
-class ModuleSource : public ModulePort{
-};
-
-class ModuleSink : public ModulePort{
-};
-
-
-class ModuleLink{
-    ModuleSource*  m_source   = nullptr;
-    ModuleSink*    m_sink     = nullptr;
-    void*          m_payload  = nullptr;
-
-public:
-    ModuleLink(ModuleSource* source, ModuleSink* sink)
-    : m_source(source), m_sink(sink) {}
-
-    inline ModuleSource* source() const { return m_source; }
-
-    inline ModuleSink* sink() const { return m_sink; }
-
-    bool isEnabled() const;
-
-    typedef void (Callback)(ModuleLink** links, unsigned int nlinks, void* arg);
-
-    static void enable(ModuleLink** links, unsigned int nlinks, ModuleLink::Callback* callback, void* arg);
-
-    static void disable(ModuleLink** links, unsigned int nlinks, ModuleLink::Callback* callback, void* arg);
+    void disengage();
 };
 
 }//namespace r64fx
