1fa12e5ae2e771550829903ccf00f7ef2a759a17
Yuri Klopovsky <yuri.klopovsky@gmail.com>
2019-09-08 16:51:06 +0300

    Declared Widget_Panel class

---
+++ src/Widget_Panel.cpp
@@ -0,0 +1,26 @@
+/* This file is part of r64fx.
+ *
+ * r64fx is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * r64fx is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with r64fx. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "Widget_Panel.hpp"
+
+namespace r64fx{
+
+Widget_Panel::Widget_Panel(Widget* parent) : Widget(parent)
+{
+
+}
+
+}//namespace r64fx
---
+++ src/Widget_Panel.hpp
@@ -0,0 +1,31 @@
+/* This file is part of r64fx.
+ *
+ * r64fx is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * r64fx is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with r64fx. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef R64FX_WIDGET_PANEL_HPP
+#define R64FX_WIDGET_PANEL_HPP
+
+#include "Widget.hpp"
+
+namespace r64fx{
+
+class Widget_Panel : public Widget{
+public:
+    Widget_Panel(Widget* parent);
+};
+
+}//namespace r64fx
+
+#endif//R64FX_WIDGET_PANEL_HPP
