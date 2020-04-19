3ddd05a852f97c890202dee2ef91fe930f6e4ed9
Yuri Klopovsky <yuri.klopovsky@gmail.com>
2020-04-20 00:35:31 +0300

    Widget_Timeline: Moved class defs to header

--- src/Widget_Timeline.cpp
+++ src/Widget_Timeline.cpp
@@ -7,54 +7,20 @@ using namespace std;
 
 namespace r64fx{
 
-class Widget_TimelineClip : public Widget{
-public:
-    Widget_TimelineClip(Widget* parent = nullptr) : Widget(parent)
-    {
-        setWidth(1024);
-    }
 
-private:
-    void paintEvent(WidgetPaintEvent* event)
-    {
-        auto p = event->painter();
-        p->strokeRect({0, 0, width(), height()}, Color(63, 32, 32), Color(255, 127, 127), 5);
-        childrenPaintEvent(event);
-    }
-};
+Widget_TimelineClip::Widget_TimelineClip(Widget* parent) : Widget(parent)
+{
+    setWidth(1024);
+}
 
-class Widget_TimelineLane : public Widget{
-public:
-    Widget_TimelineLane(Widget* parent = nullptr) : Widget(parent)
-    {
-        setHeight(256);
+Widget_TimelineLane::Widget_TimelineLane(Widget* parent) : Widget(parent)
+{
+    setHeight(256);
 
-        //REMOVE ME
-        for(int i=0; i<3; i++)
-            (void) new Widget_TimelineClip(this);
-    }
-
-private:
-    void paintEvent(WidgetPaintEvent* event)
-    {
-        auto p = event->painter();
-        p->fillRect({0, 0, width(), height() - 5}, Color(255, 223, 159));
-        childrenPaintEvent(event);
-    }
-
-    void resizeEvent(WidgetResizeEvent* event)
-    {
-        for(auto child : *this)
-        {
-            auto clip = dynamic_cast<Widget_TimelineClip*>(child);
-            if(clip)
-            {
-                clip->setPosition({0, 0});
-                clip->setHeight(height() - 5);
-            }
-        }
-    }
-};
+    //REMOVE ME
+    for(int i=0; i<3; i++)
+        (void) new Widget_TimelineClip(this);
+}
 
 Widget_Timeline::Widget_Timeline(Widget* parent) : Widget(parent)
 {
@@ -62,11 +28,40 @@ Widget_Timeline::Widget_Timeline(Widget* parent) : Widget(parent)
         (void) new Widget_TimelineLane(this);
 }
 
+
+void Widget_TimelineClip::paintEvent(WidgetPaintEvent* event)
+{
+    auto p = event->painter();
+    p->strokeRect({0, 0, width(), height()}, Color(63, 32, 32), Color(255, 127, 127), 5);
+    childrenPaintEvent(event);
+}
+
+void Widget_TimelineLane::paintEvent(WidgetPaintEvent* event)
+{
+    auto p = event->painter();
+    p->fillRect({0, 0, width(), height() - 5}, Color(255, 223, 159));
+    childrenPaintEvent(event);
+}
+
 void Widget_Timeline::paintEvent(WidgetPaintEvent* event)
 {
     auto p = event->painter();
     p->fillRect({0, 0, width(), height()}, Color(255, 191, 127));
     childrenPaintEvent(event);
+}
+
+
+void Widget_TimelineLane::resizeEvent(WidgetResizeEvent* event)
+{
+    for(auto child : *this)
+    {
+        auto clip = dynamic_cast<Widget_TimelineClip*>(child);
+        if(clip)
+        {
+            clip->setPosition({0, 0});
+            clip->setHeight(height() - 5);
+        }
+    }
 }
 
 void Widget_Timeline::resizeEvent(WidgetResizeEvent* event)
--- src/Widget_Timeline.hpp
+++ src/Widget_Timeline.hpp
@@ -5,6 +5,25 @@
 
 namespace r64fx{
 
+class Widget_TimelineClip : public Widget{
+public:
+    Widget_TimelineClip(Widget* parent = nullptr);
+
+private:
+    void paintEvent(WidgetPaintEvent* event);
+};
+
+
+class Widget_TimelineLane : public Widget{
+public:
+    Widget_TimelineLane(Widget* parent = nullptr);
+
+private:
+    void paintEvent(WidgetPaintEvent* event);
+
+    void resizeEvent(WidgetResizeEvent* event);
+};
+
 class Widget_Timeline : public Widget{
 public:
     Widget_Timeline(Widget* parent = nullptr);
